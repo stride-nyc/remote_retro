e3b05faf51b57402e00d362f2e405215bd637d3b
Matthew Lehner <matthew@mpl.io>
2015-07-17 10:36:23 -0700

    add scss support and compile CSS to separate file.

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var path = require("path");
+
 module.exports = {
-  entry: "./web/static/js/app.js",
+  entry: ["./web/static/css/app.scss", "./web/static/js/app.js"],
   output: {
-    path: "./priv/static/js",
-    filename: "app.js"
+    path: "./priv/static",
+    filename: "js/app.js"
   },
 
   module: {
@@ -11,7 +14,21 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader?" +
+                                          "includePaths[]=" +
+                                           (path.resolve(__dirname, "./node_modules")))
       }
     ]
-  }
+  },
+
+  plugins: [
+    new ExtractTextPlugin("css/app.css")
+  ]
 }
