79d191c928622727c95110c77d725afe1d69de8c
Matthew Lehner <matthew@mpl.io>
2015-07-17 09:43:29 -0700

    remove brunch config
    `rm brunch-config.js package.json`
    and comment out watcher

--- config/dev.exs
+++ config/dev.exs
@@ -10,8 +10,8 @@ config :webpack_example, WebpackExample.Endpoint,
   http: [port: 4000],
   debug_errors: true,
   code_reloader: true,
-  cache_static_lookup: false,
-  watchers: [node: ["node_modules/brunch/bin/brunch", "watch"]]
+  cache_static_lookup: false#,
+  # watchers: [node: ["node_modules/brunch/bin/brunch", "watch"]]
 
 # Watch static and templates for browser reloading.
 config :webpack_example, WebpackExample.Endpoint,
