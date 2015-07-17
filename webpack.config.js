452e19feaf91da4046d826e0f42209e9cb84168d
Matthew Lehner <matthew@mpl.io>
2015-07-17 10:25:13 -0700

    add babel
    `npm install --save-dev babel-loader babel-core`
    edit `webpack.config.js` to pipe JS through babel first.

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,15 @@ module.exports = {
   output: {
     path: "./priv/static/js",
     filename: "app.js"
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
   }
 }
