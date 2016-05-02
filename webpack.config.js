9c9ff7dcb6cc7c1d726201fb034dc9fa97187c9a
Matthew Lehner <matthew@mpl.io>
2016-05-02 10:49:50 -0700

    config matches tutorial.

--- webpack.config.js
+++ webpack.config.js
@@ -2,40 +2,31 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  devtool: "source-map",
-  entry: {
-    "app": ["./web/static/css/app.scss", "./web/static/js/app.js"],
-  },
-
+  entry: [ "./web/static/css/app.css", "./web/static/js/app.js" ],
   output: {
     path: "./priv/static",
     filename: "js/app.js"
   },
 
   resolve: {
-    modulesDirectories: [ "node_modules", __dirname + "/web/static/js" ]
+    modulesDirectories: [
+      "node_modules",
+      __dirname + "/web/static/js"
+    ]
   },
 
   module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "babel",
-        query: {
-          presets: ['es2015']
-        }
-      }, {
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract("style", "css")
-      }, {
-        test: /\.scss$/,
-        loader: ExtractTextPlugin.extract(
-          "style",
-          "css!sass?includePaths[]=" + __dirname +  "/node_modules"
-        )
+    loaders: [{
+      text: /\.js$/,
+      exclude: /node_modules/,
+      loader: "babel",
+      query: {
+        presets: ["es2015"]
       }
-    ]
+    }, {
+      test: /\.css$/,
+      loader: ExtractTextPlugin.extract("style", "css")
+    }]
   },
 
   plugins: [
