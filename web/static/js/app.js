5b1195f65a9c9a8496757b78a32e9922dde31668
Travis Vander Hoop <vanderhoop@me.com>
2016-12-11 10:57:02 -0700

    Mount top-level react component

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,3 +1,16 @@
 import "phoenix_html"
 
+import React, { Component } from 'react'
+import { render } from 'react-dom'
 
+class ReactPlayground extends Component {
+  render(){
+    return (
+      <div className="react-playground">
+        <p>The React Playground</p>
+      </div>
+    )
+  }
+}
+
+render(<ReactPlayground/>, document.querySelector('.react-root'))
