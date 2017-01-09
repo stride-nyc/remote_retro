60a2e5915d5923e21720fc18d68c8e2213b89389
Travis Vander Hoop <vanderhoop@me.com>
2017-01-09 15:12:38 -0600

    Ensure idle websocket connections are closed before 55s heroku timeout window

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,7 @@ defmodule RemoteRetro.UserSocket do
   # channel "room:*", RemoteRetro.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
