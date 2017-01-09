7475c895594a4902605799687e0afb55b5061442
Travis Vander Hoop <vanderhoop@me.com>
2017-01-09 15:05:41 -0600

    Add prod database config for heroku deploys

--- config/prod.exs
+++ config/prod.exs
@@ -18,6 +18,12 @@ config :remote_retro, RemoteRetro.Endpoint,
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
+config :remote_retro, RemoteRetro.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
+
 # Do not print debug messages in production
 config :logger, level: :info
 
