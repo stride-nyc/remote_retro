b6191d41e33b8eb3baf8a0dcb490ad4668cac219
Travis Vander Hoop <vanderhoop@me.com>
2017-01-09 15:03:36 -0600

    Reference secret key base env var for heroku deploys

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,10 @@ use Mix.Config
 # which you typically run after static files are built.
 config :remote_retro, RemoteRetro.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [scheme: "https", host: "whispering-springs-14830.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
