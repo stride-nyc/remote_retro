2b3cad35f449429cb7b2992ba52fd36148c106dd
Travis Vander Hoop <vanderhoop@me.com>
2017-01-09 17:31:24 -0600

    Enable gzip compression

--- lib/remote_retro/endpoint.ex
+++ lib/remote_retro/endpoint.ex
@@ -8,7 +8,7 @@ defmodule RemoteRetro.Endpoint do
   # You should set gzip to true if you are running phoenix.digest
   # when deploying your static files in production.
   plug Plug.Static,
-    at: "/", from: :remote_retro, gzip: false,
+    at: "/", from: :remote_retro, gzip: true,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
