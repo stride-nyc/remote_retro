24ccd8d99844b9904aba9bbc69afdbaff36256eb
Travis Vander Hoop <vanderhoop@me.com>
2017-01-17 19:55:31 -0700

    Ensure wallaby starts up during mix.test

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,3 +2,5 @@ ExUnit.start
 
 # Create the database, run migrations, and start the test transaction.
 Ecto.Adapters.SQL.Sandbox.mode(RemoteRetro.Repo, :manual)
+
+{:ok, _} = Application.ensure_all_started(:wallaby)
