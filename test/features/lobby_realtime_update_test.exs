0a48f5c09173dc4e2fc234895e8b7e5373efcd91
Travis Vander Hoop <vanderhoop@me.com>
2017-01-28 16:41:03 -0700

    Disable async feature tests
      - because the asynchronous Wallaby tests seem to share the same phantom JS server instance,
      the tests were conflicting when starting multiple browser sessions, owing to the fact that we're using websocket updates from all connecting browsers. need to do more digging to turn this back on

--- test/features/lobby_realtime_update_test.exs
+++ test/features/lobby_realtime_update_test.exs
@@ -1,5 +1,5 @@
 defmodule LobbyRealtimeUpdateTest do
-  use RemoteRetro.IntegrationCase, async: true
+  use RemoteRetro.IntegrationCase, async: false
 
   test "the realtime updates of other users entering/leaving the room", %{session: session_one} do
     join_retro_as_user(session_one, "Todd Grundy")
@@ -20,5 +20,7 @@ defmodule LobbyRealtimeUpdateTest do
     user_list_text = session_one |> find("#user-list") |> text
 
     refute String.contains?(user_list_text, "Martha Bernham")
+
+    Wallaby.end_session(session_one)
   end
 end
