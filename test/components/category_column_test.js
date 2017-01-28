0691eb48ef07858843de84dec29c52e701bf8598
Travis Vander Hoop <vanderhoop@me.com>
2017-01-28 14:14:07 -0700

    Render list of ideas as they're submitted

---
+++ test/components/category_column_test.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { shallow } from "enzyme"
+import { expect } from "chai"
+
+import CategoryColumn from "../../web/static/js/components/category_column"
+
+describe("CategoryColumn", () => {
+  it("is renders a list item for each idea in the ideas prop", () => {
+    const ideas = [{
+      body: ":confused: what happened to our CI server?"
+    }, {
+      body: ":sad: still no word on tests"
+    }]
+
+    const wrapper = shallow(<CategoryColumn ideas={ideas} />)
+    expect(wrapper.find("li")).to.have.length(2)
+  })
+})
--- web/static/js/components/room.js
+++ web/static/js/components/room.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react"
 
 import UserList from "./user_list"
+import CategoryColumn from "./category_column"
 import IdeaSubmissionForm from "./idea_submission_form"
 
 class Room extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
+    this.state = { ideas: [] }
     this.handleIdeaSubmission = this.handleIdeaSubmission.bind(this)
+    this._setupRoomChannelEventHandlers()
+  }
+
+  _setupRoomChannelEventHandlers() {
+    this.props.roomChannel.on("new_idea_received", newIdea => {
+      this.setState({ ideas: [...this.state.ideas, newIdea] })
+    })
   }
 
   handleIdeaSubmission(idea) {
@@ -17,6 +26,7 @@ class Room extends Component {
     return (
       <div>
         <UserList users={ this.props.users } />
+        <CategoryColumn ideas={ this.state.ideas }/>
         <IdeaSubmissionForm onIdeaSubmission={ this.handleIdeaSubmission }/>
       </div>
     )
