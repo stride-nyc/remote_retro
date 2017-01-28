bb9e4fc94358ec9464416b3821df98ac37ed516b
Travis Vander Hoop <vanderhoop@me.com>
2017-01-27 19:21:05 -0700

    Rename Retro component to RemoteRetro component

---
+++ web/static/js/components/remote_retro.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react"
+import { Socket, Presence } from "phoenix"
+import values from "lodash/values"
+
+import UserForm from "./user_form"
+import Retro from "./retro"
+
+class RemoteRetro extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { users: [] }
+
+    this.handleSubmitUsername = this.handleSubmitUsername.bind(this)
+  }
+
+  handleSubmitUsername(user) {
+    let socket = new Socket("/socket", {params: { user }})
+    socket.connect()
+    let presences = {}
+
+    const room = socket.channel("room:lobby")
+
+    room.on("presence_state", state => {
+      presences = Presence.syncState(presences, state)
+      const users = values(presences).map(presence => presence.user)
+      this.setState({ users })
+    })
+
+    room.on("presence_diff", diff => {
+      presences = Presence.syncDiff(presences, diff)
+      const users = values(presences).map(presence => presence.user)
+      this.setState({ users })
+    })
+
+    room.join()
+    this.setState({ user })
+  }
+
+  render() {
+    const user = this.state.user
+
+    let content
+    if (user) {
+      content = <Retro users={ this.state.users }/>
+    } else {
+      content = <UserForm onSubmitUsername={this.handleSubmitUsername} />
+    }
+
+    return <div>{ content }</div>
+  }
+}
+
+export default RemoteRetro
--- web/static/js/components/retro.js
+++ web/static/js/components/retro.js
@@ -1,52 +1,10 @@
 import React, { Component } from "react"
-import { Socket, Presence } from "phoenix"
-import values from "lodash/values"
 
-import UserForm from "./user_form"
 import UserList from "./user_list"
 
 class Retro extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { users: [] }
-
-    this.handleSubmitUsername = this.handleSubmitUsername.bind(this)
-  }
-
-  handleSubmitUsername(user) {
-    let socket = new Socket("/socket", {params: { user }})
-    socket.connect()
-    let presences = {}
-
-    const room = socket.channel("room:lobby")
-
-    room.on("presence_state", state => {
-      presences = Presence.syncState(presences, state)
-      const users = values(presences).map(presence => presence.user)
-      this.setState({ users })
-    })
-
-    room.on("presence_diff", diff => {
-      presences = Presence.syncDiff(presences, diff)
-      const users = values(presences).map(presence => presence.user)
-      this.setState({ users })
-    })
-
-    room.join()
-    this.setState({ user })
-  }
-
   render() {
-    const user = this.state.user
-
-    let content
-    if (user) {
-      content = <UserList users={ this.state.users }/>
-    } else {
-      content = <UserForm onSubmitUsername={this.handleSubmitUsername} />
-    }
-
-    return <div>{ content }</div>
+    return <UserList users={ this.props.users }/>
   }
 }
 
