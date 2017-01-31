39c268ee1b4bd2f2388fe471820f2a487854bcf8
Travis Vander Hoop <vanderhoop@me.com>
2017-01-31 05:36:27 -0700

    Enable spread operator via webpack/bable

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,8 @@ module.exports = {
       exclude: /node_modules/,
       loader: "babel",
       query: {
-        presets: ["es2015", "react"]
+        presets: ["es2015",  "react"],
+        plugins: ["transform-object-rest-spread"],
       }
     }, {
       test: /\.css$/,
