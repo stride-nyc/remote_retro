6454db9e67a4770a809e9eaad98c8914265d0023
Travis Vander Hoop <vanderhoop@me.com>
2017-02-05 18:24:37 -0600

    Persist ideas in the new_idea handler

--- web/channels/retro_channel.ex
+++ web/channels/retro_channel.ex
@@ -1,6 +1,7 @@
 defmodule RemoteRetro.RetroChannel do
   use RemoteRetro.Web, :channel
   alias RemoteRetro.Presence
+  alias RemoteRetro.Idea
 
   def join("retro:" <> retro_id, _, socket) do
     socket = Phoenix.Socket.assign(socket, :retro_id, retro_id)
@@ -17,7 +18,10 @@ defmodule RemoteRetro.RetroChannel do
   end
 
   def handle_in("new_idea", %{"body" => body, "category" => category}, socket) do
-    broadcast! socket, "new_idea_received", %{body: body, category: category }
+    changeset = Idea.changeset(%Idea{ body: body, category: category, retro_id: socket.assigns.retro_id })
+    idea = Repo.insert!(changeset)
+
+    broadcast! socket, "new_idea_received", %{ body: body, category: category }
     {:noreply, socket}
   end
 end
