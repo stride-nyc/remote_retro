6d71f670c9117a081657aea305fef5643ed964d9
Travis Vander Hoop <vanderhoop@me.com>
2017-02-06 18:48:11 -0600

    Shift indentation of multi-line pipe operations to match convention

--- test/channels/retro_channel_test.exs
+++ test/channels/retro_channel_test.exs
@@ -3,28 +3,32 @@ defmodule RemoteRetro.RetroChannelTest do
   alias RemoteRetro.RetroChannel
 
   test "the assignment of the retro_id to the socket", %{ retro: retro } do
-    { :ok, _, socket } = socket("", %{ user: "wyatt derp" })
+    { :ok, _, socket } =
+      socket("", %{ user: "wyatt derp" })
       |> subscribe_and_join(RetroChannel, "retro:" <> retro.id)
 
     assert socket.assigns.retro_id == retro.id
   end
 
   test "the push of a new presence state when a user joins the retro", %{ retro: retro } do
-    { :ok, _, _socket } = socket("", %{ user: "wyatt derp" })
+    { :ok, _, _socket } =
+      socket("", %{ user: "wyatt derp" })
       |> subscribe_and_join(RetroChannel, "retro:" <> retro.id)
 
     assert_push "presence_state", %{ "wyatt derp" => %{} }
   end
 
   test "the push of a new presence diff when a user joins the retro", %{ retro: retro } do
-    { :ok, _, _socket } = socket("", %{ user: "wyatt derp" })
+    { :ok, _, _socket } =
+      socket("", %{ user: "wyatt derp" })
       |> subscribe_and_join(RetroChannel, "retro:" <> retro.id)
 
     assert_push "presence_diff", %{ joins: %{ "wyatt derp" => %{} } }
   end
 
   test "the inclusion of a user map with metadata about their joining the retro", %{ retro: retro } do
-    { :ok, _, _socket } = socket("", %{ user: "wyatt derp" })
+    { :ok, _, _socket } =
+      socket("", %{ user: "wyatt derp" })
       |> subscribe_and_join(RetroChannel, "retro:" <> retro.id)
 
     assert_push "presence_state", %{
@@ -43,7 +47,8 @@ defmodule RemoteRetro.RetroChannelTest do
   end
 
   test "the broadcasting of new ideas to clients when pushed to the socket", %{ retro: retro } do
-    { :ok, _, socket } = socket("", %{ user: "wyatt derp" })
+    { :ok, _, socket } =
+      socket("", %{ user: "wyatt derp" })
       |> subscribe_and_join(RetroChannel, "retro:" <> retro.id)
 
     push(socket, "new_idea", %{ category: "happy", body: "we're pacing well" })
