7d8c8a4a3efbd43a0cdf569f2d2c43dcb5206400
Travis Vander Hoop <vanderhoop@me.com>
2017-02-22 19:17:36 -0700

    Displaying username!   - Add session_user to root assigns   - Conditionally display email with the appropriate button   - to set the authenticated user on the connection in the controller tests, we visit the /auth/google/endpoint, per Chris McCord's comment: https://github.com/phoenixframework/phoenix/issues/1008#issuecomment-119065067

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -1,9 +1,27 @@
 defmodule RemoteRetro.PageControllerTest do
   use RemoteRetro.ConnCase
 
-  @tag :skip
-  test "GET /", %{conn: conn} do
-    conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+  describe "authenticated visits to root" do
+    setup [:authenticate_connection]
+
+    test "invite users to create a retrospective", %{conn: conn} do
+      conn = get conn, "/"
+
+      assert Regex.match?(~r/create a retrospective/i, conn.resp_body)
+    end
+  end
+
+  describe "unauthenticated visits to root" do
+    test "asks users to authenticate", %{conn: conn} do
+      conn = get conn, "/"
+
+      refute conn.resp_body =~ ~r/create a retrospective/i
+      assert conn.resp_body =~ ~r/sign in with google/i
+    end
+  end
+
+  defp authenticate_connection(context) do
+    conn = get context[:conn], "/auth/google/callback?code=derp"
+    Map.put(context, :conn, conn)
   end
 end
--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,9 @@ defmodule RemoteRetro.PageController do
   use RemoteRetro.Web, :controller
 
   def index(conn, _params) do
+    current_user = get_session(conn, :current_user)
+
+    conn = assign(conn, :current_user, current_user)
     render conn, "index.html"
   end
 end
