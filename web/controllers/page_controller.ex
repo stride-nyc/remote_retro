bd71f8bb56a3e13c03b0bcd46dcdc4531f8c88e3
Travis Vander Hoop <vanderhoop@me.com>
2017-02-24 07:55:11 -0700

    Hotfix: add stub user to session in dev

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,7 +2,11 @@ defmodule RemoteRetro.PageController do
   use RemoteRetro.Web, :controller
 
   def index(conn, _params) do
-    current_user = get_session(conn, :current_user)
+    if Mix.env == :dev do
+      current_user = %{ "given_name" => "Todd", "family_name" => "Grundy" }
+    else
+      current_user = get_session(conn, :current_user)
+    end
 
     conn = assign(conn, :current_user, current_user)
     render conn, "index.html"
