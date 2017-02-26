4f8760ddfee165efc38caf36f8255c79abbf1e27
Travis Vander Hoop <vanderhoop@me.com>
2017-02-25 19:23:07 -0700

    Augment user map on presence objects with all items from first metas map
      - also remove now-redundant test for inclusion of first online_at value from metas

--- web/channels/presence.ex
+++ web/channels/presence.ex
@@ -4,10 +4,7 @@ defmodule RemoteRetro.Presence do
 
   def fetch(_topic, entries) do
     Enum.into entries, %{}, fn({token, presence}) ->
-      presence = Map.put(presence, :user, %{
-        online_at: List.first(presence.metas).online_at,
-        name: token,
-      })
+      presence = Map.put(presence, :user, List.first(presence.metas))
       {token, presence}
     end
   end
