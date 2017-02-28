01d980cab256b38352d4d76b8118f24a39058606
ddipanfilo <ddipanfilo@gmail.com>
2017-02-28 18:27:18 -0500

    Play chime sound when other users enter or leave room

--- test/components/room_test.js
+++ test/components/room_test.js
@@ -1,24 +1,40 @@
 import React from "react"
-import { shallow } from "enzyme"
+import { shallow, mount } from "enzyme"
 import { expect } from "chai"
 import sinon from "sinon"
 
 import Room from "../../web/static/js/components/room"
 
 describe("Room component", () => {
+  let mockRetroChannel
+
+  beforeEach(() => {
+    mockRetroChannel = { push: sinon.spy(), on: () => {} }
+  })
+
+  describe("componentWillReceiveProps", () => {
+    it("plays chime when the user list length changes", () => {
+      const roomWrapper = mount(<Room retroChannel={mockRetroChannel} users={[]} />)
+
+      const audioElement = roomWrapper.instance().chime
+      audioElement.play = sinon.spy()
+      expect(audioElement.play.called).to.equal(false)
+
+      roomWrapper.setProps({ users: [{}, {}] })
+      expect(audioElement.play.called).to.equal(true)
+    })
+  })
+
   describe(".handleIdeaSubmission", () => {
     it("pushes the idea to the room channel", () => {
-      const onStub = () => {}
-      const retroChannel = { push: sinon.spy(), on: onStub }
-
-      const roomComponent = shallow(<Room retroChannel={retroChannel} users={[]} />)
+      const roomComponent = shallow(<Room retroChannel={mockRetroChannel} users={[]} />)
 
       roomComponent
         .instance()
         .handleIdeaSubmission({ category: "sad", body: "we don't use our linter" })
 
       expect(
-        retroChannel.push.calledWith("new_idea", { category: "sad", body: "we don't use our linter" }),
+        mockRetroChannel.push.calledWith("new_idea", { category: "sad", body: "we don't use our linter" }),
       ).to.equal(true)
     })
   })
