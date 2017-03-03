b69dcac175052c2c9f63a3a76731e4002da2ff34
Travis Vander Hoop <vanderhoop@me.com>
2017-03-03 16:40:36 -0600

    Introduce PresenceUtils module for determining/setting facilitator role on longest tenured
      - add guard clause to earliest arrival helper to avoid
    Enum.EmptyError when invoking Enum.min_by with an empty map
      - note: the elixir docs suggest that there is a min_by/3 function
    with a callback for empty cases, but it’s not working with my current
    elixir (1.3.2), so to be safe: the guard clause

---
+++ test/channels/presence_utils_test.exs
@@ -0,0 +1,20 @@
+defmodule RemoteRetro.PresenceUtilsTest do
+  use ExUnit.Case, async: true
+  alias RemoteRetro.PresenceUtils
+
+  @presence_structure_with_two_users %{
+    "nme999" => %{metas: [%{online_at: 500}], user: %{}},
+    "tvh121" => %{metas: [%{online_at: 100}], user: %{}}
+  }
+
+  describe "give_facilitator_role_to_longest_tenured/1" do
+    test "sets facilitator boolean based on whether user was the earliest arrival" do
+      presences = PresenceUtils.give_facilitator_role_to_longest_tenured(
+        @presence_structure_with_two_users
+      )
+
+      assert presences["tvh121"].user.facilitator == true
+      assert presences["nme999"].user.facilitator == false
+    end
+  end
+end
---
+++ web/channels/presence_utils.ex
@@ -0,0 +1,17 @@
+defmodule RemoteRetro.PresenceUtils do
+  def give_facilitator_role_to_longest_tenured(presences) do
+    { facilitator_token, _facilitator } = earliest_arrival(presences)
+
+    Enum.into presences, %{}, fn({token, presence}) ->
+      presence = put_in(presence, [:user, :facilitator], token == facilitator_token)
+      {token, presence}
+    end
+  end
+
+  defp earliest_arrival(presences) when map_size(presences) == 0, do: nil
+  defp earliest_arrival(presences) do
+    Enum.min_by(presences, fn {_token, presence} ->
+      List.first(presence.metas).online_at
+    end)
+  end
+end
