2aa0ffef0663cd39d1c7234f1d477237937ca522
Zander Mackie <zmackie@gmail.com>
2017-03-06 20:06:29 -0500

    Turn of @spec warning from credo

--- .credo.exs
+++ .credo.exs
@@ -83,7 +83,7 @@
         {Credo.Check.Readability.PredicateFunctionNames},
         {Credo.Check.Readability.PreferImplicitTry},
         {Credo.Check.Readability.RedundantBlankLines},
-        {Credo.Check.Readability.Specs},
+        {Credo.Check.Readability.Specs, false},
         {Credo.Check.Readability.StringSigils},
         {Credo.Check.Readability.TrailingBlankLine},
         {Credo.Check.Readability.TrailingWhiteSpace},
