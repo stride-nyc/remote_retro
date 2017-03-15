56ab1883d0641d6c44258e3a6bd3f752257b8957
Charles Quirin <crq@users.noreply.github.com>
2017-03-14 20:10:37 -0400

    Remove ActionItemToggle for non-facilitators

---
+++ test/components/remote_retro_test.js
@@ -0,0 +1,48 @@
+import React from "react"
+import { shallow } from "enzyme"
+import { expect } from "chai"
+import RemoteRetro from "../../web/static/js/components/remote_retro"
+
+describe("<RemoteRetro>", () => {
+  const mockRetroChannel = {
+    on: () => {},
+    join: () => {},
+  }
+  context("when the current user is a facilitator", () => {
+    const presences = {
+      userToken: {
+        user: { facilitator: true },
+      },
+    }
+
+    it("renders a room with isFacilitiator true", () => {
+      const wrapper = shallow(
+        <RemoteRetro userToken="userToken" retroChannel={mockRetroChannel} />)
+
+      wrapper.setState({ presences })
+
+      const room = wrapper.find("Room")
+
+      expect(room.prop("isFacilitator")).to.equal(true)
+    })
+  })
+
+  context("when the current user is not a facilitator", () => {
+    const presences = {
+      userToken: {
+        user: {},
+      },
+    }
+
+    it("renders a room with isFacilitiator false", () => {
+      const wrapper = shallow(
+        <RemoteRetro userToken="userToken" retroChannel={mockRetroChannel} />)
+
+      wrapper.setState({ presences })
+
+      const room = wrapper.find("Room")
+
+      expect(room.prop("isFacilitator")).to.equal(false)
+    })
+  })
+})
