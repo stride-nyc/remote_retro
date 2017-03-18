f1c66c93ae6755cbcf163c0267b426af5b5c76ed
Travis Vander Hoop <vanderhoop@me.com>
2017-03-17 21:34:09 -0600

    Ensure javascript is compiled when invoking mix e2e

--- lib/tasks/end_to_end.ex
+++ lib/tasks/end_to_end.ex
@@ -4,10 +4,15 @@ defmodule Mix.Tasks.EndToEnd do
 
   @shortdoc "Runs feature tests"
   def run(_) do
+    compile_js
     System.cmd(
       "mix",
       ["test", "--only", "feature_test", "--color"],
       into: IO.stream(:stdio, :line)
     )
+  end
+
+  defp compile_js do
+    System.cmd("npm", ["run", "deploy", "--", "--hide-modules"], [into: IO.stream(:stdio, :line)])
   end
 end
