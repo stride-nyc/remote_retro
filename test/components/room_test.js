285bd18f6045381a387e72fd78e19c9612937009
Travis Vander Hoop <vanderhoop@me.com>
2017-03-19 17:27:29 -0600

    Add unit tests around retroChannel event handling in Room component

--- test/components/room_test.js
+++ test/components/room_test.js
@@ -1,10 +1,11 @@
 import React from "react"
-import { shallow } from "enzyme"
+import { shallow, mount } from "enzyme"
 import { expect } from "chai"
 import sinon from "sinon"
 
 import Room from "../../web/static/js/components/room"
 import CategoryColumn from "../../web/static/js/components/category_column"
+import RetroChannel from "../../web/static/js/services/retro_channel"
 
 describe("Room component", () => {
   const mockRetroChannel = { push: sinon.spy(), on: () => {} }
@@ -39,6 +40,38 @@ describe("Room component", () => {
       expect(roomComponent.containsMatchingElement(
         <CategoryColumn category="action-item" ideas={[]} />,
       )).to.equal(true)
+    })
+  })
+
+  describe("RetroChannel Events", () => {
+    let retroChannel
+    let roomComponent
+
+    beforeEach(() => {
+      retroChannel = RetroChannel.configure({})
+      roomComponent = mount(<Room retroChannel={retroChannel} users={[]} />)
+    })
+
+    it("on `existing_ideas` sets the associated payload's `ideas` value on state", () => {
+      expect(roomComponent.state("ideas")).to.eql([])
+
+      const mockPayloadFromServer = { ideas: [{ arbitrary: "content" }] }
+      retroChannel.trigger("existing_ideas", mockPayloadFromServer)
+
+      expect(roomComponent.state("ideas")).to.eql([
+        { arbitrary: "content" }
+      ])
+    })
+
+    it("pushes the value passed with `new_idea_received` into the `ideas` array", () => {
+      roomComponent.setState({ ideas: [{ body: "first idear" }]})
+
+      retroChannel.trigger("new_idea_received", { body: "zerp" })
+
+      expect(roomComponent.state("ideas")).to.eql([
+        { body: "first idear" },
+        { body: "zerp" },
+      ])
     })
   })
 })
