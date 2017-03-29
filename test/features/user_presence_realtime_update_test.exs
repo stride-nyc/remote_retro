04e1a000d17920870995c83239e1cd406257b0e1
Travis Vander Hoop <vanderhoop@me.com>
2017-03-29 08:38:36 -0500

    End-to-end test cleanup

--- test/features/user_presence_realtime_update_test.exs
+++ test/features/user_presence_realtime_update_test.exs
@@ -1,7 +1,7 @@
 defmodule UserPresenceRealtimeUpdateTest do
   use RemoteRetro.IntegrationCase, async: false
 
-  test "the realtime updates of other users entering/leaving the room", %{session: session_one, retro: retro} do
+  test "the realtime updates of other users entering/leaving a retro", %{session: session_one, retro: retro} do
     retro_path = "/retros/" <> retro.id
     session_one = visit(session_one, retro_path)
 
@@ -15,7 +15,5 @@ defmodule UserPresenceRealtimeUpdateTest do
     Wallaby.end_session(session_two)
 
     assert session_one |> find(Query.css("#user-list li", count: 1))
-
-    Wallaby.end_session(session_one)
   end
 end
