56d285fa42f69240d54a1619b2c19c8419fbb34d
Sean Marzug-McCarthy <samdec11@gmail.com>
2017-04-04 20:23:08 -0400

    DB/SM: Tests for creating action items and sending email

---
+++ test/features/send_action_item_to_users_via_email_test.exs
@@ -0,0 +1,25 @@
+defmodule SendActionItemToUsersViaEmailTest do
+  use RemoteRetro.IntegrationCase, async: false
+  use Bamboo.Test, shared: true
+
+  test "Entering an action item into the text field and sending action items delivers email", %{session: facilitator_session, retro: retro} do
+    retro_path = "/retros/" <> retro.id
+    facilitator_session = authenticate(facilitator_session) |> visit(retro_path)
+    stub_js_confirms_for_phantomjs(facilitator_session)
+
+    facilitator_session
+    |> find(Query.button("Proceed to Action Items"))
+    |> Element.click
+
+    facilitator_session
+    |> submit_idea(%{ category: "action-item", body: "DEV: Do the test" })
+
+    assert facilitator_session |> find(Query.css(".action-item li.item", count: 1))
+
+    facilitator_session
+    |> find(Query.button("Send Action Items"))
+    |> Element.click
+
+    assert_delivered_with(subject: "Action items from Retro", html_body: "DEV: Do the test")
+  end
+end
