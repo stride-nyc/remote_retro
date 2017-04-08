b7d7aebadb804ee77d3ef0ed53b2e80dd2637c99
Travis Vander Hoop <vanderhoop@me.com>
2017-04-08 17:21:43 -0600

    Convert IdeaListItem to Component definition in preparation for setChanges

--- web/static/js/components/idea_list_item.jsx
+++ web/static/js/components/idea_list_item.jsx
@@ -1,31 +1,33 @@
-import React, { PropTypes } from "react"
+import React, { Component } from "react"
 import * as AppPropTypes from "../prop_types"
 import styles from "./css_modules/idea_list_item.css"
 
-function IdeaListItem(props) {
-  const { idea, currentPresence } = props
+class IdeaListItem extends Component {
+  render() {
+    let { idea, currentPresence, handleDelete } = this.props
 
-  return (
-    <li className={styles.index} title={idea.body} key={idea.id}>
-      { currentPresence.user.is_facilitator ?
-        <i
-          id={idea.id}
-          title="Delete Idea"
-          className={styles.actionIcon + ` remove circle icon`}
-          onClick={props.handleDelete}
-        />
-        : null
-      }
-      { currentPresence.user.is_facilitator ?
-        <i
-          title="Edit Idea"
-          className={styles.actionIcon + ` edit icon`}
-        >
-        </i> : null
-      }
-      <span className={styles.authorAttribution}>{idea.author}:</span> {idea.body}
-    </li>
-  )
+    return (
+      <li className={styles.index} title={idea.body} key={idea.id}>
+        { currentPresence.user.is_facilitator ?
+          <i
+            id={idea.id}
+            title="Delete Idea"
+            className={styles.actionIcon + ` remove circle icon`}
+            onClick={props.handleDelete}
+          >
+          </i> : null
+        }
+        { currentPresence.user.is_facilitator ?
+          <i
+            title="Edit Idea"
+            className={styles.actionIcon + ` edit icon`}
+          >
+          </i> : null
+        }
+        <span className={styles.authorAttribution}>{idea.author}:</span> {idea.body}
+      </li>
+    )
+  }
 }
 
 IdeaListItem.propTypes = {
