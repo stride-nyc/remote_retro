9761c50cdc051218ffad8bbc7c39ccc83f272b9c
Sean Marzug-McCarthy <samdec11@gmail.com>
2017-04-11 20:38:53 -0400

    Only hit the db once in Emails.action_items_email/1

--- lib/remote_retro/emails.ex
+++ lib/remote_retro/emails.ex
@@ -4,22 +4,24 @@ defmodule RemoteRetro.Emails do
   alias RemoteRetro.Repo
 
   def action_items_email(retro_id) do
+    action_items = retro_action_items(retro_id)
+
     new_email(
       to: "vanderhoop@me.com",
       from: "do-not-reply@remote_retro.dev",
       subject: "Action items from Retro",
-      text_body: text_retro_action_items(retro_id),
-      html_body: html_retro_action_items(retro_id)
+      text_body: text_retro_action_items(action_items),
+      html_body: html_retro_action_items(action_items)
     )
   end
 
-  defp text_retro_action_items(retro_id) do
-    retro_action_items(retro_id)
+  defp text_retro_action_items(action_items) do
+    action_items
     |> Enum.join("\n")
   end
 
-  defp html_retro_action_items(retro_id) do
-    retro_action_items(retro_id)
+  defp html_retro_action_items(action_items) do
+    action_items
     |> Enum.join("<br/>")
   end
 
