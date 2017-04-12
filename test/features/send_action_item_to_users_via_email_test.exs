a7f9af06af4961148f38e38de9e68483bba667ad
Sean Marzug-McCarthy <samdec11@gmail.com>
2017-04-11 21:17:37 -0400

    Change query in tests because class structure on frontend changed

--- test/features/send_action_item_to_users_via_email_test.exs
+++ test/features/send_action_item_to_users_via_email_test.exs
@@ -4,6 +4,7 @@ defmodule SendActionItemToUsersViaEmailTest do
 
   test "Distributing action items via email", %{session: facilitator_session, retro: retro} do
     retro_path = "/retros/" <> retro.id
+    idea_text = "Do the test"
     facilitator_session = authenticate(facilitator_session) |> visit(retro_path)
     stub_js_confirms_for_phantomjs(facilitator_session)
 
@@ -12,14 +13,14 @@ defmodule SendActionItemToUsersViaEmailTest do
     |> Element.click
 
     facilitator_session
-    |> submit_idea(%{ category: "action-item", body: "DEV: Do the test" })
+    |> submit_idea(%{ category: "action-item", body: idea_text })
 
-    assert facilitator_session |> find(Query.css(".action-item li.item", count: 1))
+    assert facilitator_session |> find(Query.css("ul.action-item li[title='#{idea_text}']", count: 1))
 
     facilitator_session
     |> find(Query.button("Send Action Items"))
     |> Element.click
 
-    assert_delivered_with(subject: "Action items from Retro", html_body: "DEV: Do the test")
+    assert_delivered_with(subject: "Action items from Retro", html_body: idea_text)
   end
 end
