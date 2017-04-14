9f4c470f496bd954636916319293b469237c2413
Travis Vander Hoop <vanderhoop@me.com>
2017-04-14 07:36:01 -0600

    use function declaration rather than class definition for stateless component

--- web/static/js/components/idea.jsx
+++ web/static/js/components/idea.jsx
@@ -1,40 +1,39 @@
-import React, { Component } from "react"
+import React from "react"
 import IdeaControls from "./idea_controls"
 import IdeaEditForm from "./idea_edit_form"
 import * as AppPropTypes from "../prop_types"
 import styles from "./css_modules/idea.css"
 
-class Idea extends Component {
-  render() {
-    const { idea, currentPresence, handleDelete, retroChannel } = this.props
-    const isFacilitator = currentPresence.user.is_facilitator
-    let classes = styles.index
-    classes += idea.editing ? " ui raised segment" : ""
+function Idea(props) {
+  const { idea, currentPresence, handleDelete, retroChannel } = props
+  const isFacilitator = currentPresence.user.is_facilitator
+  let classes = styles.index
+  classes += idea.editing ? " ui raised segment" : ""
 
-    const readOnlyIdea = (
-      <div>
-        { idea.editing && !isFacilitator ?
-          <p>Facilitator is Editing:</p> : ""
-        }
-        { isFacilitator &&
-          <IdeaControls
-            idea={idea}
-            handleDelete={handleDelete}
-            retroChannel={retroChannel}
-          />
-        }
-        <span className={styles.authorAttribution}>{idea.author}:</span> {idea.body}
-      </div>
-    )
+  const readOnlyIdea = (
+    <div>
+      { idea.editing && !isFacilitator ?
+        <p>Facilitator is Editing:</p> : ""
+      }
+      { isFacilitator &&
+        <IdeaControls
+          idea={idea}
+          handleDelete={handleDelete}
+          retroChannel={retroChannel}
+        />
+      }
+      <span className={styles.authorAttribution}>{idea.author}:</span> {idea.body}
+    </div>
+  )
 
-    return (
-      <li className={classes} title={idea.body} key={idea.id}>
-        { idea.editing && isFacilitator ?
-          <IdeaEditForm idea={idea} retroChannel={retroChannel} /> : readOnlyIdea
-        }
-      </li>
-    )
-  }
+  return (
+    <li className={classes} title={idea.body} key={idea.id}>
+      { idea.editing && isFacilitator ?
+        <IdeaEditForm idea={idea} retroChannel={retroChannel} />
+        : readOnlyIdea
+      }
+    </li>
+  )
 }
 
 Idea.propTypes = {
