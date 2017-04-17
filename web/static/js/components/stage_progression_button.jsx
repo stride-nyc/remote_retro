a06853b647ee2944af673910f88007264e0dcb15
Travis Vander Hoop <vanderhoop@me.com>
2017-04-16 19:36:53 -0600

    StageProgressionButton#handleToggleChange -> StageProgressionButton#handleStageProgression

--- web/static/js/components/stage_progression_button.jsx
+++ web/static/js/components/stage_progression_button.jsx
@@ -4,10 +4,10 @@ import * as AppPropTypes from "../prop_types"
 class StageProgressionButton extends Component {
   constructor(props) {
     super(props)
-    this.handleToggleChange = this.handleToggleChange.bind(this)
+    this.handleStageProgression = this.handleStageProgression.bind(this)
   }
 
-  handleToggleChange() {
+  handleStageProgression() {
     if (confirm("Are you sure you would like to proceed to the action items stage?")) {
       this.props.retroChannel.push("show_action_item", { show_action_item: true })
     }
@@ -17,7 +17,7 @@ class StageProgressionButton extends Component {
     return (
       <button
         className="fluid ui right labeled teal icon button"
-        onClick={this.handleToggleChange}
+        onClick={this.handleStageProgression}
       >
         Proceed to Action Items
         <i className="arrow right icon" />
