982ae6269444361b352b47b3fb1301c9490736dd
Michael Squashic and Zander Mackie <pair+msquashic+zmackie@gmail.com>
2017-04-22 16:21:18 -0400

    removed add_mock_user_to_session

--- web/router.ex
+++ web/router.ex
@@ -11,7 +11,7 @@ defmodule RemoteRetro.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
-  
+
   end
 
   pipeline :api do
@@ -31,10 +31,5 @@ defmodule RemoteRetro.Router do
 
     get "/google", AuthController, :index
     get "/google/callback", AuthController, :callback
-  end
-
-  defp add_mock_user_to_session(conn, _options) do
-    mock_user = Application.get_env(:remote_retro, :mock_user)
-    put_session(conn, :current_user, mock_user)
   end
 end
