12bd8ba42058a05114dd35f52f771aca74bd9561
Michael Squashic <msquashic@gmail.com>
2017-04-25 18:26:56 -0400

    made changes as per @vanderhoop's comments

--- lib/tasks/preflight.ex
+++ lib/tasks/preflight.ex
@@ -8,7 +8,7 @@ defmodule Mix.Tasks.Preflight do
     check_hex_version()
   end
 
-  def check_envars do
+  defp check_envars do
     required_envars = [
       "REMOTE_RETRO_GOOGLE_OAUTH_CLIENT_ID",
       "REMOTE_RETRO_GOOGLE_OAUTH_CLIENT_SECRET",
@@ -19,20 +19,17 @@ defmodule Mix.Tasks.Preflight do
 
     num_missing = length(missing_envars)
     if num_missing > 0 do
-      IO.puts "You must define the following environment variables:\n" <> Enum.join(missing_envars, "\n")
+      IO.puts "Please define the following environment variables:\n" <> Enum.join(missing_envars, "\n")
       System.halt(:abort)
     end
   end
 
-  def check_hex_version do
+  defp check_hex_version do
     required_version_num = "0.16.0"
 
-    hex_info = "mix hex.info | grep Hex" |> String.to_char_list |> :os.cmd |> List.to_string
-    match = Regex.run(~r/(\d+\.)?(\d+\.)?(\*|\d+)/, hex_info)
-    installed_version_num = hd(match)
-
-    if installed_version_num != required_version_num do
-      IO.puts "You must install Hex #{required_version_num}"
+    { hex_version_output, _ } = System.cmd("mix", ["hex", "--version"])
+    if !String.contains?(hex_version_output, required_version_num) do
+      IO.puts "Please install Hex #{required_version_num}"
       System.halt(:abort)
     end
   end
