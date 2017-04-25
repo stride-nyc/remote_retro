ed9f2521fffaeff0ab3c6890639a2129e1f74468
Michael Squashic <msquashic@gmail.com>
2017-04-25 15:53:38 -0400

    refactored preflight.ex and added new function to enforce installation of specific hex version

--- lib/tasks/preflight.ex
+++ lib/tasks/preflight.ex
@@ -4,6 +4,11 @@ defmodule Mix.Tasks.Preflight do
 
   @shortdoc "Checks for necessary ENVARS"
   def run(_) do
+    check_envars()
+    check_hex_version()
+  end
+
+  def check_envars do
     required_envars = [
       "REMOTE_RETRO_GOOGLE_OAUTH_CLIENT_ID",
       "REMOTE_RETRO_GOOGLE_OAUTH_CLIENT_SECRET",
@@ -15,6 +20,19 @@ defmodule Mix.Tasks.Preflight do
     num_missing = length(missing_envars)
     if num_missing > 0 do
       IO.puts "You must define the following environment variables:\n" <> Enum.join(missing_envars, "\n")
+      System.halt(:abort)
+    end
+  end
+
+  def check_hex_version do
+    required_version_num = "0.16.0"
+
+    hex_info = "mix hex.info | grep Hex" |> String.to_char_list |> :os.cmd |> List.to_string
+    match = Regex.run(~r/(\d+\.)?(\d+\.)?(\*|\d+)/, hex_info)
+    installed_version_num = hd(match)
+
+    if installed_version_num != required_version_num do
+      IO.puts "You must install Hex #{required_version_num}"
       System.halt(:abort)
     end
   end
