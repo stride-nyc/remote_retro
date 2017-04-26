37b3212b466cd465b36ffd5f934292189be274a3
Travis Vander Hoop <vanderhoop@me.com>
2017-04-26 17:43:41 -0600

    Enable out-of-the-box JSON encoding of retro model

--- web/models/retro.ex
+++ web/models/retro.ex
@@ -3,6 +3,7 @@ defmodule RemoteRetro.Retro do
   use RemoteRetro.Web, :model
 
   @primary_key {:id, :binary_id, autogenerate: true}
+  @derive {Poison.Encoder, except: [:__meta__, :participations]}
 
   schema "retros" do
     has_many :participations, RemoteRetro.Participation
