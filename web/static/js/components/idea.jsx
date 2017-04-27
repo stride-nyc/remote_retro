f8dd25c4329d23dd3031dad5f82e5b4519349b94
Travis Vander Hoop <vanderhoop@me.com>
2017-04-26 21:25:49 -0600

    Reinstate space between idea and '(edited)' indicator

--- web/static/js/components/idea.jsx
+++ web/static/js/components/idea.jsx
@@ -25,7 +25,7 @@ function Idea(props) {
       <span className={styles.authorAttribution}>
         {idea.author}:
       </span> {idea.liveEditText || idea.body}
-      {isEdited && <span className={styles.editedIndicator}>(edited)</span>}
+      {isEdited && <span className={styles.editedIndicator}> (edited)</span>}
     </div>
   )
 
