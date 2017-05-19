3647eba27128bccac76c74eb3379dae22c957d86
Aldric Giacomoni <trevoke@gmail.com>
2017-05-18 21:52:22 -0400

    Clean up AuthController
    Fix #136

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -1,35 +1,20 @@
 defmodule RemoteRetro.AuthController do
   use RemoteRetro.Web, :controller
-  alias RemoteRetro.OAuth.Google
-  alias RemoteRetro.User
+
+  alias RemoteRetro.OAuth
 
   def index(conn, _params) do
     redirect conn, external: authorize_url!()
   end
 
   def callback(conn, %{"code" => code}) do
-    user_info = Google.get_user_info!(code)
-    user_params = User.build_user_from_oauth(user_info)
-
-    {:ok, user} =
-      case Repo.get_by(User, email: user_info["email"]) do
-        nil -> %User{}
-        user_from_db -> user_from_db
-      end
-      |> User.changeset(user_params)
-      |> Repo.insert_or_update
-
-    user =
-      user
-      |> Map.delete(:__meta__)
-      |> Map.delete(:__struct__)
-
+    user = OAuth.Google.retrieve_internal_user(code)
     conn = put_session(conn, :current_user, user)
 
     redirect conn, to: get_session(conn, "requested_endpoint") || "/"
   end
 
   defp authorize_url! do
-    Google.authorize_url!(scope: "email profile")
+    OAuth.Google.authorize_url!(scope: "email profile")
   end
 end
--- web/oauth/google.ex
+++ web/oauth/google.ex
@@ -4,8 +4,27 @@ defmodule RemoteRetro.OAuth.Google do
   """
 
   use OAuth2.Strategy
+  alias RemoteRetro.User
+  alias RemoteRetro.Repo
 
   @oauth_client Application.get_env(:remote_retro, :oauth_client)
+
+  def retrieve_internal_user(code) do
+    user_info = get_user_info!(code)
+    user_params = build_user_from_oauth(user_info)
+
+    {:ok, user} =
+      case Repo.get_by(User, email: user_info["email"]) do
+        nil -> %User{}
+        user_from_db -> user_from_db
+      end
+      |> User.changeset(user_params)
+      |> Repo.insert_or_update
+
+    user
+    |> Map.delete(:__meta__)
+    |> Map.delete(:__struct__)
+  end
 
   def authorize_url!(params) do
     @oauth_client.authorize_url!(client(), params)
@@ -35,5 +54,15 @@ defmodule RemoteRetro.OAuth.Google do
       authorize_url: "https://accounts.google.com/o/oauth2/auth",
       token_url: "https://accounts.google.com/o/oauth2/token"
     ])
+  end
+
+  defp build_user_from_oauth(user_info) do
+    user_params = %{
+      "email" => user_info["email"],
+      "google_user_info"=> user_info,
+      "last_login"=> DateTime.utc_now
+    }
+
+    Map.merge(user_params, user_info)
   end
 end
