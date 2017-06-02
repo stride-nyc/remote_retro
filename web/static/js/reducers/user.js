6eb246d3a5940b2a88f8e9826f8c532dc323ddd9
Travis Vander Hoop <vanderhoop@me.com>
2017-06-02 06:34:18 -0600

    Refactor: use .map with equality check rather than immutability helpers with index

--- web/static/js/reducers/user.js
+++ web/static/js/reducers/user.js
@@ -1,14 +1,11 @@
-import update from "immutability-helper"
-
 const user = (state = [], action) => {
   switch (action.type) {
   case "ADD_USERS":
     return action.users
   case "UPDATE_USER":
     const { userToken, newAttributes } = action
-    const index = state.findIndex(user => user.token === userToken)
-    return update(state, {
-      [index]: { $set: { ...state[index], ...newAttributes } }
+    return state.map(user => {
+      return (user.token === userToken) ? { ...user, ...newAttributes } : user
     })
   default:
     return state
