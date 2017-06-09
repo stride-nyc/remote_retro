5424cefe5a51592ac4e2ee4d70f38dd831a6def8
Travis Vander Hoop <vanderhoop@me.com>
2017-06-08 21:07:59 -0600

    Set ideas from action ideas outright

--- web/static/js/reducers/idea.js
+++ web/static/js/reducers/idea.js
@@ -1,7 +1,7 @@
 const idea = (state = [], action) => {
   switch (action.type) {
     case "SET_IDEAS":
-      return [...action.ideas]
+      return action.ideas
     case "ADD_IDEA":
       return [...state, action.idea]
     default:
