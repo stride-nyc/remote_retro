bd308f4bdb55cea3d34455819f7b04a9335d6328
Travis Vander Hoop <vanderhoop@me.com>
2017-06-15 16:42:24 -0600

    Add idea reducer to root reducer

--- web/static/js/reducers/index.js
+++ web/static/js/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux"
 
 import user from "./user"
+import idea from "./idea"
 
 const rootReducer = combineReducers({
   user,
+  idea,
 })
 
 export default rootReducer
