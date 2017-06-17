f11389d8217ff24ba6b354a28edf013523d63e9d
Travis Vander Hoop <vanderhoop@me.com>
2017-06-17 11:08:47 -0600

    Refactor UserListItem, leveraging destructuring of arguments to avoid duplication

--- web/static/js/components/user_list_item.jsx
+++ web/static/js/components/user_list_item.jsx
@@ -2,19 +2,19 @@ import React from "react"
 import * as AppPropTypes from "../prop_types"
 import styles from "./css_modules/user_list_item.css"
 
-const UserListItem = props => {
-  let userName = props.user.given_name
-  const imgSrc = props.user.picture.replace("sz=50", "sz=200")
+const UserListItem = ({ user }) => {
+  let givenName = user.given_name
+  const imgSrc = user.picture.replace("sz=50", "sz=200")
 
-  if (props.user.is_facilitator) userName += " (Facilitator)"
+  if (user.is_facilitator) givenName += " (Facilitator)"
   return (
     <li className={`item ${styles.wrapper}`}>
       <div className="ui center aligned grid">
-        <img className={styles.picture} src={imgSrc} alt={props.user.given_name} />
+        <img className={styles.picture} src={imgSrc} alt={givenName} />
         <div className="ui row">
-          <p className={styles.name}>{ userName }</p>
+          <p className={styles.name}>{ givenName }</p>
           <p className={`${styles.ellipsisAnim} ui row`}>
-            { props.user.is_typing &&
+            { user.is_typing &&
               <span>
                 <i className="circle icon" />
                 <i className="circle icon" />
