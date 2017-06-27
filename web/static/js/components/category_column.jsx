e7766e5bc4b743d86d1b122c30a8d1a2a41a1546
Travis Vander Hoop <vanderhoop@me.com>
2017-06-27 16:27:28 -0600

    Refactor: leverage spread syntax in passing props from one component to child component

--- web/static/js/components/category_column.jsx
+++ web/static/js/components/category_column.jsx
@@ -4,18 +4,11 @@ import * as AppPropTypes from "../prop_types"
 import styles from "./css_modules/category_column.css"
 
 const CategoryColumn = props => {
-  const { category, ideas, currentUser, retroChannel } = props
+  const { category, ideas } = props
   const filteredIdeas = ideas.filter(idea => idea.category === category)
   const sortedIdeas = filteredIdeas.sort((a, b) => a.id - b.id)
   const iconHeight = 45
-  const ideasList = sortedIdeas.map(idea => (
-    <Idea
-      idea={idea}
-      key={idea.id}
-      currentUser={currentUser}
-      retroChannel={retroChannel}
-    />
-  ))
+  const ideasList = sortedIdeas.map(idea => <Idea {...props} idea={idea} key={idea.id} />)
 
   return (
     <section className={`${category} ${styles.index} column`}>
@@ -24,18 +17,14 @@ const CategoryColumn = props => {
         <p><strong>{category}</strong></p>
       </div>
       <div className={`ui fitted divider ${styles.divider}`} />
-      <ul className={`${category} ${styles.list} ideas`}>
-        {ideasList}
-      </ul>
+      <ul className={`${category} ${styles.list} ideas`}>{ideasList}</ul>
     </section>
   )
 }
 
 CategoryColumn.propTypes = {
   ideas: AppPropTypes.ideas.isRequired,
-  currentUser: AppPropTypes.user.isRequired,
   category: AppPropTypes.category.isRequired,
-  retroChannel: AppPropTypes.retroChannel.isRequired,
 }
 
 export default CategoryColumn
