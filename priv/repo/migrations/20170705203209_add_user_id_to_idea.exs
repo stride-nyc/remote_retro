d965282bb0de057c1b3248d72c744968db16eb21
Claire Dranginis and Tanya Newell <pair+claire+tanya@stridenyc.com>
2017-07-06 10:54:21 -0400

    Create migration to add user_id column to ideas table, models, and test

---
+++ priv/repo/migrations/20170705203209_add_user_id_to_idea.exs
@@ -0,0 +1,12 @@
+defmodule RemoteRetro.Repo.Migrations.AddUserIdToIdea do
+  use Ecto.Migration
+
+  def change do
+    alter table(:ideas) do
+      add :user_id, references(:users, [
+        column: :id,
+        on_delete: :nothing
+      ])
+    end
+  end
+end
