11a4c9b6635894c30261184fa3362fa126c21429
Travis Vander Hoop <vanderhoop@me.com>
2017-07-30 09:29:18 -0600

    UpdateState action creator passes along stageProgressionConfigs

--- web/static/js/actions/retro.js
+++ web/static/js/actions/retro.js
@@ -1,6 +1,9 @@
+import stageProgressionConfigs from "../configs/stage_progression_configs"
+
 export const updateStage = newStage => ({
   type: "UPDATE_STAGE",
   stage: newStage,
+  stageProgressionConfigs,
 })
 
 export const setInitialState = initialState => ({
