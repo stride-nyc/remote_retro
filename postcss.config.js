a6e760562e13bb0a6dc675ad3795e70303c686f2
Travis Vander Hoop <vanderhoop@me.com>
2017-08-01 18:10:26 -0600

    Revert addition of postcss-nested to build pipeline
    This reverts commit b00f308fe0f38c44198e4552ec1893ed6bb2c481 and  493859e4703a42cd2fed3c8cfb5e5256c9171dcf.

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,7 @@
 const postCssCssVariables = require("postcss-css-variables")
-const postCssNested = require("postcss-nested")
 
 module.exports = {
   plugins: [
     postCssCssVariables,
-    postCssNested,
   ],
 }
--- webpack.config.js
+++ webpack.config.js
@@ -47,11 +47,8 @@ module.exports = {
               importLoaders: 1,
               localIdentName: '[name]__[local]___[hash:base64:5]'
             }
-          },
-          {
-            loader: 'postcss-loader'
-          }
-        ]
+         }
+       ]
       }),
     }]
   },
