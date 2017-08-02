a3ac939bf357dfdedc10a7206248c20b39e5f684
Travis Vander Hoop <vanderhoop@me.com>
2017-08-01 21:27:03 -0600

    Refactor: remove unnecessary db hit

--- web/controllers/retro_controller.ex
+++ web/controllers/retro_controller.ex
@@ -1,6 +1,6 @@
 defmodule RemoteRetro.RetroController do
   use RemoteRetro.Web, :controller
-  alias RemoteRetro.{Retro, Participation, User}
+  alias RemoteRetro.{Retro, Participation}
   alias Phoenix.Token
 
   def show(conn, params) do
@@ -9,8 +9,7 @@ defmodule RemoteRetro.RetroController do
         conn = put_session conn, "requested_endpoint", conn.request_path
         redirect conn, to: "/auth/google"
       user ->
-        user_from_db = Repo.get_by(User, email: user.email)
-        query = from p in Participation, where: p.user_id == ^user_from_db.id and p.retro_id == ^params["id"]
+        query = from p in Participation, where: p.user_id == ^user.id and p.retro_id == ^params["id"]
         changeset = Participation.changeset(%Participation{}, %{
           user_id: user.id,
           retro_id: params["id"]
