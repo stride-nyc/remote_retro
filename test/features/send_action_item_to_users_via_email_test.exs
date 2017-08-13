d848aa0c7c66d50c4f22ce2dc6de1cbed4a04755
Travis Vander Hoop <vanderhoop@me.com>
2017-08-13 08:30:42 -0600

    Build hotfix: update stage progression e2e tests such that there are persisted ideas before bootstrap
      - upon adding a disabling condition to the StageProgressionButton during the ideas stage (that it is disabled if no ideas exist), we broke the build because the e2e tests weren't updated to accommodate the condition

--- test/features/send_action_item_to_users_via_email_test.exs
+++ test/features/send_action_item_to_users_via_email_test.exs
@@ -1,19 +1,27 @@
 defmodule SendActionItemToUsersViaEmailTest do
-  alias RemoteRetro.Emails
+  alias RemoteRetro.{Emails, Idea}
   use RemoteRetro.IntegrationCase, async: false
   use Bamboo.Test, shared: true
 
-  test "Distributing action items via email", %{session: facilitator_session, retro: retro} do
-    retro_path = "/retros/" <> retro.id
-    idea_text = "Do the test"
-    facilitator_session = authenticate(facilitator_session) |> visit(retro_path)
-    click_and_confirm(facilitator_session, "Proceed to Action Items")
+  @mock_user Application.get_env(:remote_retro, :mock_user)
 
-    submit_idea(facilitator_session, %{category: "action-item", body: idea_text})
+  describe "when an idea already exists in a retro" do
+    setup [:persist_user_for_retro, :persist_idea_for_retro]
 
-    assert facilitator_session |> find(Query.css("ul.action-item li[title='#{idea_text}']", count: 1))
-    click_and_confirm(facilitator_session, "Send Action Items")
+    @tag user: Map.put(@mock_user, "email", "travy@davy.com")
+    @tag idea: %Idea{category: "happy", body: "Teams worked well together"}
+    test "Distributing action items via email", %{session: facilitator_session, retro: retro} do
+      retro_path = "/retros/" <> retro.id
+      idea_text = "Do the test"
+      facilitator_session = authenticate(facilitator_session) |> visit(retro_path)
+      click_and_confirm(facilitator_session, "Proceed to Action Items")
 
-    Emails.action_items_email(retro.id) |> assert_delivered_email
+      submit_idea(facilitator_session, %{category: "action-item", body: idea_text})
+
+      assert facilitator_session |> find(Query.css("ul.action-item li[title='#{idea_text}']", count: 1))
+      click_and_confirm(facilitator_session, "Send Action Items")
+
+      Emails.action_items_email(retro.id) |> assert_delivered_email
+    end
   end
 end
--- test/features/stage_progression_realtime_update_test.exs
+++ test/features/stage_progression_realtime_update_test.exs
@@ -1,18 +1,26 @@
 defmodule StageProgressionRealtimeUpdateTest do
   use RemoteRetro.IntegrationCase, async: false
 
-  test "realtime stage progression for connected users", %{session: facilitator_session, retro: retro} do
-    participant_session = new_browser_session()
+  @mock_user Application.get_env(:remote_retro, :mock_user)
 
-    retro_path = "/retros/" <> retro.id
-    facilitator_session = authenticate(facilitator_session) |> visit(retro_path)
-    participant_session = authenticate(participant_session) |> visit(retro_path)
-    stub_js_confirms_for_phantomjs(facilitator_session)
+  describe "when an idea already exists in a retro" do
+    setup [:persist_user_for_retro, :persist_idea_for_retro]
 
-    assert participant_session |> find(Query.css(".action-item.column", count: 0))
+    @tag user: Map.put(@mock_user, "email", "travy@davy.com")
+    @tag idea: %RemoteRetro.Idea{category: "happy", body: "Teams worked well together"}
+    test "realtime stage progression for connected users", %{session: facilitator_session, retro: retro} do
+      participant_session = new_browser_session()
 
-    click_and_confirm(facilitator_session, "Proceed to Action Items")
+      retro_path = "/retros/" <> retro.id
+      facilitator_session = authenticate(facilitator_session) |> visit(retro_path)
+      participant_session = authenticate(participant_session) |> visit(retro_path)
+      stub_js_confirms_for_phantomjs(facilitator_session)
 
-    assert participant_session |> find(Query.css(".action-item.column", count: 1))
+      assert participant_session |> find(Query.css(".action-item.column", count: 0))
+
+      click_and_confirm(facilitator_session, "Proceed to Action Items")
+
+      assert participant_session |> find(Query.css(".action-item.column", count: 1))
+    end
   end
 end
