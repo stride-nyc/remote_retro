b9b61b037e09949678e60f23adaeaeaa2d761398
Travis Vander Hoop <vanderhoop@me.com>
2017-08-24 09:33:51 -0600

    Hotfix: use mount in test that looks for content in ReactTransitionGroup

--- test/components/vote_counter_test.js
+++ test/components/vote_counter_test.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { shallow } from "enzyme"
+import { shallow, mount } from "enzyme"
 import { spy } from "sinon"
 
 import VoteCounter from "../../web/static/js/components/vote_counter"
@@ -17,7 +17,7 @@ describe("VoteCounter", () => {
   const mockUser = { id: 55 }
 
   it("renders an anchor tag that contains the vote count of the idea", () => {
-    const voteCounter = shallow(
+    const voteCounter = mount(
       <VoteCounter
         retroChannel={{}}
         idea={idea}
