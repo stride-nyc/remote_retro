0fb9a91918058e897eeb41beca255a9fe0ba1e94
Travis Vander Hoop <vanderhoop@me.com>
2017-09-10 14:06:11 -0600

    Extract color variables out into own file. Introduce postcss import dep

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,10 @@
+const postCssImport = require("postcss-import")
 const postCssNested = require("postcss-nested")
 const postCssCssVariables = require("postcss-css-variables")
 
 module.exports = {
   plugins: [
+    postCssImport,
     postCssNested,
     postCssCssVariables(),
   ],
