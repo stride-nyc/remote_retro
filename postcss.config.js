29b9851b9e7d4ba6f91fd9d73911b594b76d769d
Travis Vander Hoop <vanderhoop@me.com>
2017-09-10 09:59:58 -0600

    Bugfix: ensure css variables are transpiled to fix b0rked styles in IE

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,9 @@
 const postCssNested = require("postcss-nested")
+const postCssCssVariables = require("postcss-css-variables")
 
 module.exports = {
   plugins: [
     postCssNested,
+    postCssCssVariables,
   ],
 }
