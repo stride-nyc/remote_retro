3f00be081d13f7647d622555f35407644ebfacb8
Travis Vander Hoop <vanderhoop@me.com>
2017-09-16 11:45:22 -0600

    Shift AnimatedEllipsis from span to div to ensure 100% width

--- web/static/js/components/animated_ellipsis.jsx
+++ web/static/js/components/animated_ellipsis.jsx
@@ -3,11 +3,11 @@ import styles from "./css_modules/animated_ellipsis.css"
 
 const AnimatedEllipsis = ({ display }) => {
   return (
-    <span className={styles.index} style={{ opacity: display ? 1 : 0 }} >
+    <div className={styles.index} style={{ opacity: display ? 1 : 0 }} >
       <i className="circle icon" />
       <i className="circle icon" />
       <i className="circle icon" />
-    </span>
+    </div>
   )
 }
 
