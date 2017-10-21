60a5a430e4d445180e0307111c20cd8c93e2fd3f
Travis Vander Hoop <vanderhoop@me.com>
2017-10-20 21:46:44 -0600

    End to end tests always compile javascript, but without minification/deduping, to remain in parity with dev server assets
      - allows developers to run end-to-end tests while the server (which leverages webpack-dev-server assets) is running. Webpack dev server serves assets from memory, while the test suite expects js assets to be available on the file system. We introduce a new compile-dev npm script to ensure we have the non-minified assets on the file system

--- lib/tasks/end_to_end.ex
+++ lib/tasks/end_to_end.ex
@@ -1,9 +1,10 @@
 defmodule Mix.Tasks.EndToEnd do
   use Mix.Task
 
-  @shortdoc "Runs feature tests"
+  @shortdoc "Runs end-to-end tests"
   def run(_) do
-    compile_js_if_necessary()
+    compile_js()
+    IO.puts "\n#{IO.ANSI.green()}Executing end-to-end tests...#{IO.ANSI.reset()}\n"
     System.cmd(
       "mix",
       ["test", "test/features/", "--only", "feature_test", "--color"],
@@ -11,9 +12,7 @@ defmodule Mix.Tasks.EndToEnd do
     )
   end
 
-  defp compile_js_if_necessary do
-    if !File.exists?("priv/static/js/dll.vendor.js") do
-      System.cmd("npm", ["run", "deploy", "--", "--hide-modules"], [into: IO.stream(:stdio, :line)])
-    end
+  defp compile_js do
+    System.cmd("npm", ["run", "compile-dev", "--", "--hide-modules"], [into: IO.stream(:stdio, :line)])
   end
 end
