39062f83971eb60df4ca40da636e44327afd3cfb
Travis Vander Hoop <vanderhoop@me.com>
2017-10-20 20:44:25 -0600

    Refactor: extract conditional serving of app.js file out into view helper
      - the webpack dev server serves the app.js bundle in dev, while the default phoenix server hosts the bundle in other environments

--- test/views/layout_view_test.exs
+++ test/views/layout_view_test.exs
@@ -1,3 +1,16 @@
 defmodule RemoteRetro.LayoutViewTest do
   use RemoteRetro.ConnCase, async: true
+  alias RemoteRetro.LayoutView
+
+  test "app_js is served by the webpack dev server (at port 5001) in dev" do
+    Mix.env(:dev)
+    conn = get build_conn(), "/"
+    assert LayoutView.app_js(conn) =~ "localhost:5001"
+    Mix.env(:test)
+  end
+
+  test "app_js is served by a default host at path js/app.js in other environments" do
+    conn = get build_conn(), "/"
+    assert LayoutView.app_js(conn) =~ "/js/app.js"
+  end
 end
--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,3 +1,10 @@
 defmodule RemoteRetro.LayoutView do
   use RemoteRetro.Web, :view
+
+  def app_js(conn) do
+    case Mix.env do
+      :dev -> "http://localhost:5001/js/app.js"
+      _ -> static_path(conn, "/js/app.js")
+    end
+  end
 end
