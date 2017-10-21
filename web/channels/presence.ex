b4c55ad1442e59a78cf4bd8fdbc02df2f897dc83
Travis Vander Hoop <vanderhoop@me.com>
2017-10-21 11:57:42 -0600

    Credo Lint

--- web/channels/presence.ex
+++ web/channels/presence.ex
@@ -6,7 +6,7 @@ defmodule RemoteRetro.Presence do
                         pubsub_server: RemoteRetro.PubSub
 
   def fetch(_topic, entries) do
-    entries = Enum.into entries, %{}, fn({user_token, presence}) ->
+    Enum.into entries, %{}, fn({user_token, presence}) ->
       user_metadata = List.first(presence.metas)
       presence = Map.put(presence, :user, Map.put(user_metadata, :token, user_token))
       {user_token, presence}
