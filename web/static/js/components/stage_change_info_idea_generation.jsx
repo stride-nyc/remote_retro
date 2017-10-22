ea18dfdecfdf7055c157d92367e30f097766c67a
Travis Vander Hoop <vanderhoop@me.com>
2017-10-22 10:15:46 -0600

    Upate copy of StageChangeInfoIdeaGeneration

--- web/static/js/components/stage_change_info_idea_generation.jsx
+++ web/static/js/components/stage_change_info_idea_generation.jsx
@@ -5,9 +5,12 @@ export default () => (
     The skinny on Idea Generation:
     <div className="ui basic segment">
       <ul className="ui list">
-        <li>Reflect on the events of this past sprint/iteration. What happened, and when?</li>
+        <li>Reflect on the events of this past sprint. What happened, and when?</li>
         <li>Submit items that made you happy, sad, or just plain confused.</li>
-        <li>Don't throw anyone under the bus. If a disaster occurred, leave the names at the door.</li>
+        <li>
+          Don't throw anyone under the bus.
+          If a disaster occurred, leave the names at the door.
+        </li>
       </ul>
     </div>
   </div>
