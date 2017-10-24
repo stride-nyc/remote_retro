191d7fb3a094266ecdc792c915a04e16429c442f
Travis Vander Hoop <vanderhoop@me.com>
2017-10-24 17:13:32 -0600

    Only enable redux dev tools in dev

--- web/static/js/configure_store.js
+++ web/static/js/configure_store.js
@@ -7,7 +7,7 @@ import interceptOverEagerReactReduxWarning from "./dev-utils/intercept_overeager
 export default () => {
   const store = createStore(
     rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__ && module.hot && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 
   // ensures that updates to reducers are hot reloaded
