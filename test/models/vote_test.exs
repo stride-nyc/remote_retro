fd0f25ee4e005ce9ea2d4002b025cea57b83456c
Travis Vander Hoop <vanderhoop@me.com>
2017-10-24 17:30:55 -0600

    Add Vote model

---
+++ test/models/vote_test.exs
@@ -0,0 +1,15 @@
+defmodule RemoteRetro.VoteTest do
+  use RemoteRetro.ModelCase, async: true
+
+  alias RemoteRetro.Vote
+
+  test "user_id and idea_id are required" do
+    changeset = Vote.changeset(%Vote{})
+
+    { user_id_error, _ } = Keyword.fetch!(changeset.errors, :user_id)
+    { idea_id_error, _ } = Keyword.fetch!(changeset.errors, :idea_id)
+
+    assert user_id_error == "can't be blank"
+    assert idea_id_error == "can't be blank"
+  end
+end
---
+++ web/models/vote.ex
@@ -0,0 +1,19 @@
+defmodule RemoteRetro.Vote do
+  use RemoteRetro.Web, :model
+
+  @derive {Poison.Encoder, except: [:__meta__]}
+  schema "participations" do
+    belongs_to :user, RemoteRetro.User
+    belongs_to :idea, RemoteRetro.Idea
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @allowed_fields [:user_id, :idea_id]
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, @allowed_fields)
+    |> validate_required([:user_id, :idea_id])
+  end
+end
