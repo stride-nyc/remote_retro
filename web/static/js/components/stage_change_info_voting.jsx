67e726a42a81e20a456e623ac099ba82281c30fe
Tanya Newell <tnewell@stride.nyc>
2017-11-14 17:43:42 -0500

    Only display other users' vote counts after the voting phase

--- web/static/js/components/stage_change_info_voting.jsx
+++ web/static/js/components/stage_change_info_voting.jsx
@@ -7,6 +7,7 @@ export default () => (
       <ul className="ui list">
         <li>You have <strong>5</strong> votes</li>
         <li>You can apply multiple votes to a given idea</li>
+        <li>Voting is blind. Totals will be revealed when the facilitator advances the retro.</li>
         <li>Once a vote has been cast, there's no taking it back, so vote carefully!</li>
       </ul>
     </div>
