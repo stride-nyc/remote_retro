fe16fb493dd2c6f077861b4870849ae04c7ef5f9
Ian McNally <ianmcnally@gmail.com>
2017-12-15 12:31:49 -0500

    Refactor <Idea> to classical component
    - will allow it to be rendered inside a flip component

--- web/static/js/components/idea.jsx
+++ web/static/js/components/idea.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { Component } from "react"
 import classNames from "classnames"
 
 import IdeaEditForm from "./idea_edit_form"
@@ -7,33 +7,35 @@ import IdeaReadOnlyContent from "./idea_read_only_content"
 import * as AppPropTypes from "../prop_types"
 import styles from "./css_modules/idea.css"
 
-const Idea = props => {
-  const { idea, currentUser, retroChannel, stage } = props
-  const classes = classNames(styles.index, {
-    [styles.highlighted]: idea.isHighlighted,
-  })
+export default class Idea extends Component {
+  render() {
+    const { idea, currentUser, retroChannel, stage } = this.props
+    const classes = classNames(styles.index, {
+      [styles.highlighted]: idea.isHighlighted,
+    })
 
-  const userIsEditing = idea.editing && idea.editorToken === currentUser.token
+    const userIsEditing = idea.editing && idea.editorToken === currentUser.token
 
-  let content
-  if (userIsEditing) {
-    content = (<IdeaEditForm
-      idea={idea}
-      retroChannel={retroChannel}
-      currentUser={currentUser}
-      stage={stage}
-    />)
-  } else if (idea.liveEditText) {
-    content = <IdeaLiveEditContent idea={idea} />
-  } else {
-    content = <IdeaReadOnlyContent {...props} />
+    let content
+    if (userIsEditing) {
+      content = (<IdeaEditForm
+        idea={idea}
+        retroChannel={retroChannel}
+        currentUser={currentUser}
+        stage={stage}
+      />)
+    } else if (idea.liveEditText) {
+      content = <IdeaLiveEditContent idea={idea} />
+    } else {
+      content = <IdeaReadOnlyContent {...this.props} />
+    }
+
+    return (
+      <li className={classes} title={idea.body} key={idea.id}>
+        { content }
+      </li>
+    )
   }
-
-  return (
-    <li className={classes} title={idea.body} key={idea.id}>
-      { content }
-    </li>
-  )
 }
 
 Idea.propTypes = {
@@ -43,4 +45,3 @@ Idea.propTypes = {
   stage: AppPropTypes.stage.isRequired,
 }
 
-export default Idea
