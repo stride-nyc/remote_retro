a9cedad44aa58a47700108a7199e14253a4b0551
Travis Vander Hoop <vanderhoop@me.com>
2018-01-09 16:59:29 -0700

    usersById reducer handles SET_USERS action

--- test/reducers/users_by_id_test.js
+++ test/reducers/users_by_id_test.js
@@ -16,6 +16,8 @@ describe("usersById reducer", () => {
     describe("and there is initial state", () => {
       it("should return that initial state", () => {
         const initialState = { id: 1 }
+        deepFreeze(initialState)
+
         const unhandledAction = { type: "IHAVENOIDEAWHATSHAPPENING" }
 
         expect(usersById(initialState, {})).to.deep.equal(initialState)
@@ -44,22 +46,67 @@ describe("usersById reducer", () => {
       })
     })
 
+    describe("when the action is SET_USERS", () => {
+      describe("when presence state datastructure is empty", () => {
+        const action = {
+          type: "SET_USERS",
+          users: [
+            { id: 6, name: "Kevin" },
+            { id: 7, name: "Blurg Man" },
+          ],
+        }
+
+        it("is populated with the users passed, their ids becoming state keys", () => {
+          const initialState = {}
+          deepFreeze(initialState)
+
+          expect(usersById(initialState, action)).to.deep.equal({
+            6: { id: 6, name: "Kevin" },
+            7: { id: 7, name: "Blurg Man" },
+          })
+        })
+      })
+
+      describe("when presence state datastructure contains users already on state", () => {
+        const action = {
+          type: "SET_USERS",
+          users: [
+            { id: 6, name: "Kevin" },
+            { id: 7, name: "Blurg Man" },
+          ],
+        }
+
+        it("merges the users info into the usersById datastructure", () => {
+          const initialState = {
+            7: { id: 7, name: "Travis" },
+          }
+
+          deepFreeze(initialState)
+
+          expect(usersById(initialState, action)).to.deep.equal({
+            6: { id: 6, name: "Kevin" },
+            7: { id: 7, name: "Blurg Man" },
+          })
+        })
+      })
+    })
+
     describe("when the action is SYNC_PRESENCE_DIFF", () => {
       describe("when presenceDiff contains 'joins'", () => {
         const action = {
           type: "SYNC_PRESENCE_DIFF",
           presenceDiff: {
             joins: {
-                ABC: { user: { id: 60, name: "Kevin" } },
-                XYZ: { user: { id: 61, name: "Sarah" } },
-              },
-              leaves: {},
+              ABC: { user: { id: 60, name: "Kevin" } },
+              XYZ: { user: { id: 61, name: "Sarah" } },
+            },
+            leaves: {},
           },
         }
 
         it("adds users who have 'joined' to the state", () => {
           const initialState = {
-            5: { id: 5, name: "Travis" }
+            5: { id: 5, name: "Travis" },
           }
 
           expect(usersById(initialState, action)).to.deep.equal({
--- web/static/js/reducers/users_by_id.js
+++ web/static/js/reducers/users_by_id.js
@@ -1,13 +1,16 @@
 import keyBy from "lodash/keyBy"
 
-export default (state = {}, { type, initialState, presenceDiff }) => {
-  switch (type) {
+export default (state = {}, action) => {
+  switch (action.type) {
     case "SET_INITIAL_STATE":
-      return keyBy(initialState.users, "id");
-    case "SYNC_PRESENCE_DIFF":
-      const presencesRepresentingJoins = Object.values(presenceDiff.joins)
+      return keyBy(action.initialState.users, "id")
+    case "SET_USERS":
+      return Object.assign(state, keyBy(action.users, "id"))
+    case "SYNC_PRESENCE_DIFF": {
+      const presencesRepresentingJoins = Object.values(action.presenceDiff.joins)
       const users = presencesRepresentingJoins.map(join => join.user)
       return Object.assign(state, keyBy(users, "id"))
+    }
     default:
       return state
   }
