12f6a65380de3385faae7485f550f3154ccfa4ca
Travis Vander Hoop <vanderhoop@me.com>
2018-01-13 16:06:03 -0700

    Use 'slash' import from lodash and add module to vendor dll

--- web/static/js/components/select_dropdown.jsx
+++ web/static/js/components/select_dropdown.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { capitalize } from "lodash"
+import capitalize from "lodash/capitalize"
 import PropTypes from "prop-types"
 
 import styles from "./css_modules/idea_submission_form.css"
