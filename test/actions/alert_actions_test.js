5967e77ce3f58c1dc6985f1969961e543da29d54
Claire Dranginis <cdranginis@gmail.com>
2018-02-01 15:42:15 -0500

    Add tests for alert action creator and reducer

--- test/actions/alert_actions_test.js
+++ test/actions/alert_actions_test.js
@@ -5,3 +5,10 @@ describe("clearAlert", () => {
     expect(actionCreators.clearAlert()).to.deep.equal({ type: "CLEAR_ALERT" })
   })
 })
+
+describe("changeFacilitator", () => {
+  it("creates an action to change alert text to facilitation transfer info", () => {
+    const previousFacilitatorName = "Jane"
+    expect(actionCreators.changeFacilitator(previousFacilitatorName)).to.deep.equal({ type: "CHANGE_FACILITATOR", previousFacilitatorName })
+  })
+})
