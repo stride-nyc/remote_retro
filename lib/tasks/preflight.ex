31350ce92d27e45f6d1f8fdf299182496d01e4c2
nichol <nichol@ncaxp.fios-router.home>
2018-02-15 15:56:22 -0500

    remove check_envars function from preflight

--- lib/tasks/preflight.ex
+++ lib/tasks/preflight.ex
@@ -3,25 +3,7 @@ defmodule Mix.Tasks.Preflight do
 
   @shortdoc "Checks for necessary ENVARS"
   def run(_) do
-    check_envars()
     check_hex_version()
-  end
-
-  defp check_envars do
-    # required_envars = [
-    #   "REMOTE_RETRO_GOOGLE_OAUTH_CLIENT_ID",
-    #   "REMOTE_RETRO_GOOGLE_OAUTH_CLIENT_SECRET",
-    #   "REMOTE_RETRO_GOOGLE_OAUTH_REDIRECT_URI"
-    # ]
-
-    # missing_envars = Enum.reject(required_envars, &System.get_env/1)
-
-    # num_missing = length(missing_envars)
-    # if num_missing > 0 do
-    #   IO.puts "The following environment variables need to be defined:\n\t#{Enum.join(missing_envars, "\n\t")}"
-    #   IO.puts "Please refer to the README.md file for detailed instructions on setting this up."
-    #   System.halt(:abort)
-    # end
   end
 
   defp check_hex_version do
