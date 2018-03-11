0937217732ac701c703d3360fafb2898c86d699f
Travis Vander Hoop <vanderhoop@me.com>
2018-03-10 19:59:34 -0700

    Add migration that adds user_id and retro_id indexes on participations
      - avoids sequential scan on user_id when getting all retros for a user

---
+++ priv/repo/migrations/20180311025803_add-retro-id-and-user-id-indexes-on-participations.exs
@@ -0,0 +1,8 @@
+defmodule :"Elixir.RemoteRetro.Repo.Migrations.Add-retro-id-and-user-id-indexes-on-participations" do
+  use Ecto.Migration
+
+  def change do
+    create index(:participations, [:retro_id])
+    create index(:participations, [:user_id])
+  end
+end
