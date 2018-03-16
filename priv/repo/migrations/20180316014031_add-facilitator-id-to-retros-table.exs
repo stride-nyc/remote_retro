cd6b568e36f3bf55c302394c0ee892a450fd6e19
Travis Vander Hoop <vanderhoop@me.com>
2018-03-15 19:45:46 -0600

    Add retros.facilitator_id foreign key, referencing users table

---
+++ priv/repo/migrations/20180316014031_add-facilitator-id-to-retros-table.exs
@@ -0,0 +1,12 @@
+defmodule :"Elixir.RemoteRetro.Repo.Migrations.Add-facilitator-id-to-retros-table" do
+  use Ecto.Migration
+
+  def change do
+    alter table(:retros) do
+      add :facilitator_id, references(:users, [
+        column: :id,
+        on_delete: :nothing
+      ])
+    end
+  end
+end
