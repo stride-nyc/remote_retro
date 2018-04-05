ce052c0cbab845df658d676f32e76869ee22159e
Travis Vander Hoop <vanderhoop@me.com>
2018-04-05 08:04:45 -0600

    Update Voting stage intro copy

--- web/static/js/components/stage_change_info_voting.jsx
+++ web/static/js/components/stage_change_info_voting.jsx
@@ -5,9 +5,9 @@ export default () => (
     The skinny on voting:
     <div className="ui basic segment">
       <ul className="ui list">
-        <li>You have <strong>3</strong> votes</li>
-        <li>You can apply multiple votes to a given idea</li>
-        <li>Voting is blind. Totals will be revealed when the facilitator advances the retro.</li>
+        <li>You have <strong>3</strong> votes.</li>
+        <li>You can apply <strong>multiple</strong> votes to a single idea.</li>
+        <li>Voting is <strong>blind</strong>. Totals will be revealed when the facilitator advances the retro.</li>
         <li>Once a vote has been cast, there's no taking it back, so vote carefully!</li>
       </ul>
     </div>
