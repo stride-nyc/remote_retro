e41863326efff2ecd59d683cc1ef51080aff0ba7
Travis Vander Hoop <vanderhoop@me.com>
2018-06-30 21:24:48 -0500

    Lint

--- web/static/js/components/stage_change_info_voting.jsx
+++ web/static/js/components/stage_change_info_voting.jsx
@@ -7,7 +7,10 @@ export default () => (
       <ul className="ui list">
         <li>You have <strong>3</strong> votes.</li>
         <li>You can apply <strong>multiple</strong> votes to a single idea.</li>
-        <li>Voting is <strong>blind</strong>. Totals will be revealed when the facilitator advances the retro.</li>
+        <li>
+          Voting is <strong>blind</strong>.
+          Totals will be revealed when the facilitator advances the retro.
+        </li>
         <li>Once a vote has been cast, there's no taking it back, so vote carefully!</li>
       </ul>
     </div>
