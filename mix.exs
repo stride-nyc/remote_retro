4a69c6347e628dd0d0e3a5e913d2c43d89cbf98d
Travis Vander Hoop <vanderhoop@me.com>
2018-07-14 09:46:29 -0400

    Make dependency on phoenix 1.3.3 explicit

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule RemoteRetro.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.3.2"},
+    [{:phoenix, "~> 1.3.3"},
      {:phoenix_pubsub, "~> 1.0.2"},
      {:phoenix_ecto, "~> 3.3.0"},
      {:distillery, "~> 1.5.0"},
