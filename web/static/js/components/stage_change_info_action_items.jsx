b00c732570ed4cfad5587811f32c30e98d087a24
Travis Vander Hoop <vanderhoop@me.com>
2018-07-16 20:53:44 -0400

    Update action items stage prompt such that folks put their laptops away

--- web/static/js/components/stage_change_info_action_items.jsx
+++ web/static/js/components/stage_change_info_action_items.jsx
@@ -5,21 +5,16 @@ export default () => (
     The skinny on Action-Item Generation:
     <div className="ui basic segment">
       <ul className="ui list">
-        <li>Discuss the highest-voted items on the board.
-          <ul>
-            <li>
-              <small>
-                <strong>Protip: </strong>
-                let discussions breathe, conducting root cause analyses where appropriate.
-              </small>
-            </li>
-          </ul>
-        </li>
+        <li>Discuss the highest-voted items on the board.</li>
         <li>Generate action-items aimed at:
           <ul>
             <li>exploding the team's bottlenecks</li>
             <li>bolstering the team's successes</li>
           </ul>
+        </li>
+        <li>
+          If you're physically present in the room with the facilitator, put
+          your laptop away so you can focus.
         </li>
       </ul>
     </div>
