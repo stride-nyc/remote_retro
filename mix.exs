2f27a92bb93311cb13874de3b3477379b58e561d
Travis Vander Hoop <vanderhoop@me.com>
2018-08-11 12:04:30 -0400

    Bump phoenix_pubsub 1.0.2 -> 1.1.0

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule RemoteRetro.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [{:phoenix, "~> 1.3.3"},
-     {:phoenix_pubsub, "~> 1.0.2"},
+     {:phoenix_pubsub, "~> 1.1.0"},
      {:phoenix_ecto, "~> 3.3.0"},
      {:distillery, "~> 1.5.0", only: :prod},
      {:excoveralls, "~> 0.9.1", only: :test},
