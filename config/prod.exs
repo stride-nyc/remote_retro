bafabcc90242f0585f0aad69af6d7348aae036eb
Travis Vander Hoop <vanderhoop@me.com>
2018-08-16 18:06:41 +0100

    Build: correct cache_manifest file name

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ config :remote_retro, RemoteRetroWeb.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "remoteretro.org", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json",
+  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: "${SECRET_KEY_BASE}"
 
 config :remote_retro, RemoteRetro.Repo,
