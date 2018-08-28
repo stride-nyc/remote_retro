f355e3caa58e967e1af272a6690f533ba2e1939f
Travis Vander Hoop <vanderhoop@me.com>
2018-08-28 10:31:43 +0100

    Remove hex --version from preflight check
      - this was put in place to keep mix.lock generation consistent across machines, and, while it provided value, that value doesn't outweight the maintenance cost

--- lib/tasks/preflight.ex
+++ lib/tasks/preflight.ex
@@ -4,7 +4,6 @@ defmodule Mix.Tasks.Preflight do
   @shortdoc "Checks for necessary ENVARS"
   def run(_) do
     check_envars()
-    check_hex_version()
   end
 
   defp check_envars do
@@ -20,22 +19,6 @@ defmodule Mix.Tasks.Preflight do
     if num_missing > 0 do
       IO.puts "The following environment variables need to be defined:\n\t#{Enum.join(missing_envars, "\n\t")}"
       IO.puts "Please refer to the README.md file for detailed instructions on setting this up."
-      System.halt(:abort)
-    end
-  end
-
-  defp check_hex_version do
-    required_version_num = ~r/0\.17\.\d+/
-
-    {hex_version_output, _} = System.cmd("mix", ["hex", "--version"])
-    if !Regex.match?(required_version_num, hex_version_output) do
-      IO.puts """
-
-        Greetings Developer,
-        Looks like your local Hex is outdated. Please update your local Hex via `mix local.hex` and try again.
-
-        Thanks!
-      """
       System.halt(:abort)
     end
   end
