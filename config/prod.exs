03da9aeaae780a7c8d1ae1bc13e5c329efc2d386
Travis Vander Hoop <vanderhoop@me.com>
2018-09-23 18:44:01 +0200

    Distillery pulls host name production config from ENV

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ use Mix.Config
 config :remote_retro, RemoteRetroWeb.Endpoint,
   server: true,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "remoteretro.org", port: 443],
+  url: [scheme: "https", host: "${HOST}", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: "${SECRET_KEY_BASE}"
