db6af936b12a22b6e30c32fd21baf2977f6a08c0
Travis Vander Hoop <vanderhoop@me.com>
2018-09-24 14:16:02 +0200

    Strip 'icon' class from lower third in 'closed' stage; provided unnecessary margin

--- web/static/js/components/closed_lower_third_content.jsx
+++ web/static/js/components/closed_lower_third_content.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const ClosedLowerThirdContent = () => (
   <div className="ui stackable grid basic attached secondary center aligned segment">
-    <div className="ui column icon header">
+    <div className="ui column header">
       <div className="content">
         This retro is all wrapped up!
         <p className="sub header">Contents are read-only.</p>
