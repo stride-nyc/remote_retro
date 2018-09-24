87692a8ec97f2e800b52a5b9682d7a9028816875
Travis Vander Hoop <vanderhoop@me.com>
2018-09-24 14:13:33 +0200

    Ensure lower third retains padding on mobile during 'closed' stage

--- web/static/js/components/closed_lower_third_content.jsx
+++ web/static/js/components/closed_lower_third_content.jsx
@@ -2,10 +2,10 @@ import React from "react"
 
 const ClosedLowerThirdContent = () => (
   <div className="ui stackable grid basic attached secondary center aligned segment">
-    <div className="ui row icon header">
+    <div className="ui column icon header">
       <div className="content">
-        This retro is all wrapped up.
-        <p className="sub header">The contents are read-only, and available solely for review.</p>
+        This retro is all wrapped up!
+        <p className="sub header">Contents are read-only.</p>
       </div>
     </div>
   </div>
