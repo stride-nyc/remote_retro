e52e824275d7136390c078dda9dde95df3000ae6
Travis Vander Hoop <vanderhoop@me.com>
2019-01-23 08:07:59 -0600

    Surface 'Visit Your Dashboard' link in closed stage's lower third

--- web/static/js/components/closed_lower_third_content.jsx
+++ web/static/js/components/closed_lower_third_content.jsx
@@ -1,13 +1,21 @@
 import React from "react"
 
+import styles from "./css_modules/closed_lower_third_content.css"
+
 const ClosedLowerThirdContent = () => (
   <div className="ui stackable grid basic attached secondary center aligned segment">
-    <div className="ui column header">
+    <div className="ui four wide column padded header">
       <div className="content">
         This retro is all wrapped up!
         <p className="sub header">Contents are read-only.</p>
       </div>
     </div>
+    <a href="/retros" className={styles.dashboardLink}>
+      <button className="ui right labeled gray icon button">
+        Visit your dashboard
+        <i className="arrow right icon" />
+      </button>
+    </a>
   </div>
 )
 
