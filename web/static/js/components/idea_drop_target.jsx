586f57b382840a5707c96612e0b495d6ba57432a
Travis Vander Hoop <vanderhoop@me.com>
2019-02-10 11:30:48 -0600

    Optimization: throttle invocations of handleDragOver's state updates
      - onDragOver gets invoked repeatedly for as long as the user keeps hovering over the element
      - we need to *always* call event.preventDefault, but updates aren't necessary every few milliseconds

--- web/static/js/components/idea_drop_target.jsx
+++ web/static/js/components/idea_drop_target.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
 import PropTypes from "prop-types"
 import classNames from "classnames"
+import throttle from "lodash/throttle"
 
 require("./css_modules/idea_drop_target.css")
 
@@ -8,15 +9,31 @@ export class IdeaDropTarget extends Component {
   state = {}
 
   handleDragOver = event => {
-    this.setState({ draggedOver: true })
+    // must always be called to ensure drop events fire
+    // https://stackoverflow.com/questions/19133225/is-call-to-preventdefault-really-necessary-on-drop-event
     event.preventDefault()
+
+    // dragOver fires every few ms, even when the mouse *isn't* moving, so we debounce
+    this._debouncedUpdateToDraggedOverTrue()
+  }
+
+  _debouncedUpdateToDraggedOverTrue = throttle(() => {
+    this.setState({ draggedOver: true })
+  }, 100)
+
+  _setDraggedOverToFalse = () => {
+    // cancel any queued updates triggered by the dragOver handler so that
+    // draggedOver doesn't get set to true *after* being set to false
+    // https://lodash.com/docs/4.17.11#throttle
+    this._debouncedUpdateToDraggedOverTrue.cancel()
+    this.setState({ draggedOver: false })
   }
 
   handleDragLeave = event => {
     const { currentTarget, relatedTarget } = event
     if (currentTarget.contains(relatedTarget)) { return }
 
-    this.setState({ draggedOver: false })
+    this._setDraggedOverToFalse()
   }
 
   handleDrop = event => {
@@ -25,8 +42,8 @@ export class IdeaDropTarget extends Component {
     // account for unrelated drops triggered by the browser, such as images
     if (!ideaData) { return }
 
-    this.setState({ draggedOver: false })
     event.preventDefault()
+    this._setDraggedOverToFalse()
 
     const idea = JSON.parse(ideaData)
 
