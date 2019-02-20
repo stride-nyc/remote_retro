08685b679cfe99e287a2d31fa9ed5b9e7dc2e0ec
Travis Vander Hoop <vanderhoop@me.com>
2019-02-20 08:53:24 -0600

    Add GitHub star prompt to 'closed' stage's lower third

--- web/static/js/components/closed_lower_third_content.jsx
+++ web/static/js/components/closed_lower_third_content.jsx
@@ -1,10 +1,11 @@
+/* eslint-disable react/no-danger */
 import React from "react"
 
 import styles from "./css_modules/closed_lower_third_content.css"
 
 const ClosedLowerThirdContent = () => (
   <div className="ui stackable grid basic attached secondary center aligned segment">
-    <div className="ui four wide column padded header">
+    <div className="ui four wide column header">
       <div className="content">
         This retro is all wrapped up!
         <p className="sub header">Contents are read-only.</p>
@@ -16,7 +17,16 @@ const ClosedLowerThirdContent = () => (
         <i className="arrow right icon" />
       </button>
     </a>
+
+    <span
+      className={`${styles.githubStarPrompt}`}
+      dangerouslySetInnerHTML={{
+        __html: "<iframe src='https://ghbtns.com/github-btn.html?user=stride-nyc&amp;repo=remote_retro&amp;type=watch&amp;count=true&size=large' allowtransparency='true' frameborder='0' scrolling='0' width='160px' height='30px'></iframe>",
+      }}
+    />
   </div>
 )
+
+/* eslint-enable react/no-danger */
 
 export default ClosedLowerThirdContent
