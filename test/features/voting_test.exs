501265e4a1507a6b01cb810082f3de179b73bda3
Travis Vander Hoop <vanderhoop@me.com>
2019-02-21 13:56:22 -0600

    Add end-to-end test for incrementing/decrementing new experience
      - update assert_vote_count_is helper to use waiting Wallaby assertion to tests wait for async process to complete

--- test/features/voting_test.exs
+++ test/features/voting_test.exs
@@ -4,12 +4,14 @@ defmodule VotingTest do
 
   import ShorterMaps
 
+  @category "happy"
+
   describe "voting for an idea" do
     setup [:persist_idea_for_retro]
 
     @tag [
       retro_stage: "voting",
-      idea: %Idea{category: "happy", body: "pacing well"},
+      idea: %Idea{category: @category, body: "pacing well"},
     ]
 
     test "increments the idea's vote count across sessions", ~M{retro, session: session_one} do
@@ -21,15 +23,40 @@ defmodule VotingTest do
 
       session_one |> assert_vote_count_is(0)
 
-      session_one |> find(Query.css(".happy.column .green.button")) |> Element.click
+      session_one |> find(Query.css(".#{@category}.column .green.button")) |> Element.click
 
       session_one |> assert_vote_count_is(1)
       session_two |> assert_vote_count_is(1)
     end
+
+    @tag [
+      retro_stage: "voting",
+      idea: %Idea{category: @category, body: "pacing well"},
+    ]
+    test "incrementing/decrementing an idea's vote count across sessions", ~M{retro, session: session_one} do
+      session_two = new_authenticated_browser_session()
+      execute_script(session_one, "window.localStorage.setItem('subtractVoteDev', true)")
+      execute_script(session_two, "window.localStorage.setItem('subtractVoteDev', true)")
+
+      retro_path = "/retros/" <> retro.id
+      session_one = visit(session_one, retro_path)
+      session_two = visit(session_two, retro_path)
+
+      session_one |> assert_vote_count_is(0)
+
+      session_one |> find(Query.css(".#{@category}.column .plus.button")) |> Element.click
+
+      session_one |> assert_vote_count_is(1)
+      session_two |> assert_vote_count_is(1)
+
+      session_one |> find(Query.css(".#{@category}.column .minus.button")) |> Element.click
+
+      session_one |> assert_vote_count_is(0)
+      session_two |> assert_vote_count_is(0)
+    end
   end
 
   defp assert_vote_count_is(session, vote_count) do
-    body_text = session |> find(Query.css("body")) |> Element.text()
-    assert body_text =~ ~r/vote\n#{vote_count}/i
+    assert_has(session, Query.css(".#{@category}.column", text: "#{vote_count}"))
   end
 end
