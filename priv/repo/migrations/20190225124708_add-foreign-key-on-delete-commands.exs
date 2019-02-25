6a348fe512a79305f44cf84fdb454ce36308cd9b
Travis Vander Hoop <vanderhoop@me.com>
2019-02-25 07:01:02 -0600

    Introduce reversible database migration that handles on_delete of related records
      - Nullify ideas.assignee_id when assignee is deleted
      - Cascade deletion of participations when their associated (retro|user) is deleted
      - Cascade deletion of votes when their associated (user|idea) is deleted

---
+++ priv/repo/migrations/20190225124708_add-foreign-key-on-delete-commands.exs
@@ -0,0 +1,105 @@
+defmodule :"Elixir.RemoteRetro.Repo.Migrations.Add-foreign-key-on-delete-commands" do
+  use Ecto.Migration
+
+  def up do
+    drop constraint(:retros, "retros_facilitator_id_fkey")
+    alter table("retros") do
+      modify :facilitator_id, references(:users, [
+        column: :id,
+        on_delete: :nilify_all
+      ])
+    end
+
+    drop constraint(:ideas, "ideas_assignee_id_fkey")
+    alter table("ideas") do
+      modify :assignee_id, references(:users, [
+        column: :id,
+        on_delete: :nilify_all
+      ])
+    end
+
+    drop constraint(:participations, "participations_user_id_fkey")
+    alter table("participations") do
+      modify :user_id, references(:users, [
+        column: :id,
+        on_delete: :delete_all
+      ])
+    end
+
+    drop constraint(:participations, "participations_retro_id_fkey")
+    alter table("participations") do
+      modify :retro_id, references(:retros, [
+        column: :id,
+        type: :uuid,
+        on_delete: :delete_all
+      ])
+    end
+
+    drop constraint(:votes, "votes_user_id_fkey")
+    alter table("votes") do
+      modify :user_id, references(:users, [
+        column: :id,
+        on_delete: :delete_all
+      ])
+    end
+
+    drop constraint(:votes, "votes_idea_id_fkey")
+    alter table("votes") do
+      modify :idea_id, references(:ideas, [
+        column: :id,
+        on_delete: :delete_all
+      ])
+    end
+  end
+
+  def down do
+    drop constraint(:retros, "retros_facilitator_id_fkey")
+    alter table("retros") do
+      modify :facilitator_id, references(:users, [
+        column: :id,
+        on_delete: :nothing
+      ])
+    end
+
+    drop constraint(:ideas, "ideas_assignee_id_fkey")
+    alter table("ideas") do
+      modify :assignee_id, references(:users, [
+        column: :id,
+        on_delete: :nothing
+      ])
+    end
+
+    drop constraint(:participations, "participations_user_id_fkey")
+    alter table("participations") do
+      modify :user_id, references(:users, [
+        column: :id,
+        on_delete: :nothing
+      ])
+    end
+
+    drop constraint(:participations, "participations_retro_id_fkey")
+    alter table("participations") do
+      modify :retro_id, references(:retros, [
+        column: :id,
+        type: :uuid,
+        on_delete: :nothing
+      ])
+    end
+
+    drop constraint(:votes, "votes_user_id_fkey")
+    alter table("votes") do
+      modify :user_id, references(:users, [
+        column: :id,
+        on_delete: :nothing
+      ])
+    end
+
+    drop constraint(:votes, "votes_idea_id_fkey")
+    alter table("votes") do
+      modify :idea_id, references(:ideas, [
+        column: :id,
+        on_delete: :nothing
+      ])
+    end
+  end
+end
