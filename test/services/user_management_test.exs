b662b1a6f7ec4ea2a6c5272f31259f2ab780d3da
Travis Vander Hoop <vanderhoop@me.com>
2019-03-02 06:50:59 -0600

    Refactor: shift UserManagement tests to bamboo assertions over spys
      - something in using the Mock module causes significant delays

--- test/services/user_management_test.exs
+++ test/services/user_management_test.exs
@@ -2,7 +2,7 @@ defmodule RemoteRetroWeb.UserManagementTest do
   use ExUnit.Case, async: false
   use Bamboo.Test, shared: true
 
-  alias RemoteRetro.{User, Mailer}
+  alias RemoteRetro.{User}
   alias RemoteRetroWeb.{UserManagement}
 
   import Mock
@@ -11,10 +11,6 @@ defmodule RemoteRetroWeb.UserManagementTest do
 
   # The syntax for with_mocks gets very bracey/bracketey, so we break the mock declarations
   # out into private helper methods, allowing us to spy on the stubbed functions within the test
-  defp mock_deliver_now do
-    {Mailer, [], [deliver_now: fn(_) -> %{} end]}
-  end
-
   defp mock_upsert_record_from(inserted_or_updated \\ :inserted) do
     {User, [], [upsert_record_from: fn(oauth_info: _) ->
       mock_user = %User{email: "grant@me.com", given_name: "John Dillinger"}
@@ -24,7 +20,7 @@ defmodule RemoteRetroWeb.UserManagementTest do
 
   describe ".handle_google_oauth" do
     test "it calls the user model's insert_or_update/1 func" do
-      with_mocks [mock_deliver_now(), mock_upsert_record_from()] do
+      with_mocks [mock_upsert_record_from()] do
         UserManagement.handle_google_oauth(@mock_google_user_info)
 
         assert_called User.upsert_record_from(oauth_info: %{})
@@ -32,18 +28,18 @@ defmodule RemoteRetroWeb.UserManagementTest do
     end
 
     test "triggers an email when the user comes back as inserted" do
-      with_mocks [mock_deliver_now(), mock_upsert_record_from(:inserted)] do
+      with_mocks [mock_upsert_record_from(:inserted)] do
         {:ok, _user} = UserManagement.handle_google_oauth(@mock_google_user_info)
 
-        assert_called Mailer.deliver_now(%{})
+        assert_delivered_with(%{to: [nil: "grant@me.com"]})
       end
     end
 
     test "does *not* trigger an email when the user comes back as updated" do
-      with_mocks [mock_deliver_now(), mock_upsert_record_from(:updated)] do
+      with_mocks [mock_upsert_record_from(:updated)] do
         {:ok, _user} = UserManagement.handle_google_oauth(@mock_google_user_info)
 
-        refute called(Mailer.deliver_now(%{}))
+        assert_no_emails_delivered()
       end
     end
   end
