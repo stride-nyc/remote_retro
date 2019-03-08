0c4d8aef810845282203dce47d18b5ffe7cedd36
Travis Vander Hoop <vanderhoop@me.com>
2019-03-07 21:36:50 -0600

    Add end-to-end test for drag of idea from one column to another
      - introduce drag_idea test helper

---
+++ test/features/category_change_via_drag_and_drop_test.exs
@@ -0,0 +1,25 @@
+defmodule CategoryChangeViaDragAndDrop do
+  use RemoteRetro.IntegrationCase, async: false
+  alias RemoteRetro.Idea
+
+  import ShorterMaps
+
+  describe "when an idea already exists in a retro" do
+    setup [:persist_idea_for_retro]
+
+    @tag [
+      idea: %Idea{category: "sad", body: "no linter"},
+    ]
+    test "users can drag the idea from one column to another", ~M{retro, session} do
+      retro_path = "/retros/" <> retro.id
+      session = visit(session, retro_path)
+
+      assert_has(session, Query.css(".sad.ideas", text: "no linter"))
+
+      session |> drag_idea("no linter", from: "sad", to: "confused")
+
+      # assert other client sees persistence indicator
+      assert_has(session, Query.css(".confused.column", text: "no linter"))
+    end
+  end
+end
