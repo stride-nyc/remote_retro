11b7686a80a0e93752c04e9db9b517536792f67a
Cody Duval <cody.duval@gmail.com>
2019-03-14 07:29:02 -0400

    Validate presence of assignee_id on Idea when an action-item

--- lib/remote_retro_web/models/idea.ex
+++ lib/remote_retro_web/models/idea.ex
@@ -28,5 +28,15 @@ defmodule RemoteRetro.Idea do
     struct
     |> cast(params, @mutable_fields)
     |> validate_required(@required_fields)
+    |> validate_assignee_required_for_action_items()
+  end
+
+  def validate_assignee_required_for_action_items(changeset) do
+    category = get_field(changeset, :category)
+
+    case category do
+      "action-item" -> validate_required(changeset, :assignee_id)
+      _ -> changeset
+    end
   end
 end
