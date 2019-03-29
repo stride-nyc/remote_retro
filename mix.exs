d8695f0083cd0b8c5c4bc0f02e2d662973adde53
Travis Vander Hoop <vanderhoop@me.com>
2019-03-29 09:57:09 -0400

    Bump timex to bring in latest tzdata for version check fix

--- mix.exs
+++ mix.exs
@@ -58,7 +58,7 @@ defmodule RemoteRetro.Mixfile do
       {:honeybadger, "~> 0.10.1"},
       {:credo, "~> 0.3", only: [:dev, :test]},
       {:apex, "~>1.2.0", only: [:dev, :test]},
-      {:timex, "~> 3.1"},
+      {:timex, "~> 3.5"},
       {:jason, "~> 1.1.2"},
     ]
   end
