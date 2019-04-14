92868130c0336401854f15ea5b85dd53fa854eda
Travis Vander Hoop <vanderhoop@me.com>
2019-04-13 22:08:30 -0600

    Remove credo; prefer Elixir formatter

--- mix.exs
+++ mix.exs
@@ -56,7 +56,6 @@ defmodule RemoteRetro.Mixfile do
       {:libcluster, "~> 2.5.0"},
       {:bamboo, "~> 1.2"},
       {:honeybadger, "~> 0.10.1"},
-      {:credo, "~> 0.3", only: [:dev, :test]},
       {:apex, "~>1.2.1", only: [:dev, :test]},
       {:timex, "~> 3.5"},
       {:jason, "~> 1.1.2"},
