ded1ddeb3b466ef351bdddbbbbb8e3e001e8f072
Travis Vander Hoop <vanderhoop@me.com>
2019-05-06 07:21:31 -0600

    Bump jquery depedency to bring in security patch
      - add end-to-end test for first-time user experience
        - this functionality was mostly covered by lower level integration tests, but in changing our jquery dependency, I recognized a blind spot that it didn't make sense not to cover, as a change in the jQuery script (that the new user experience relies upon) could in theory break this feature without the lower level tests catching it

---
+++ test/features/first_time_user_experience_test.exs
@@ -0,0 +1,32 @@
+defmodule FirstTimeUserExperienceTest do
+  use RemoteRetro.IntegrationCase, async: false
+  alias RemoteRetro.{Participation}
+
+  import ShorterMaps
+
+
+  describe "a user with no prior retro experience" do
+    setup [:remove_any_prior_participations_in_retros]
+
+    test "is welcomed as a new user who can create a retrospective", ~M{session} do
+      visit(session, "/retros")
+
+      assert_has(session, Query.css("body", text: "Welcome, Test!"))
+      assert_user_can_create_their_first_retrospective(session)
+    end
+  end
+
+  defp remove_any_prior_participations_in_retros(%{facilitator: user} = context) do
+    user_id = user.id
+    from(p in Participation, where: p.user_id == ^user_id) |> Repo.delete_all
+
+    context
+  end
+
+  defp assert_user_can_create_their_first_retrospective(session) do
+    # the blue retro creation button isn't visible on page load due to javascript-managed delay
+    session
+    |> click(Query.css("form .blue.button[type='submit']", visible: false))
+    |> assert_has(Query.css("body", text: "Share the retro link below with teammates!"))
+  end
+end
