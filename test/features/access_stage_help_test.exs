c0b4a34edf4f861047da1469afd09545063e9795
Travis Vander Hoop <vanderhoop@me.com>
2019-05-22 10:37:25 -0600

    Add end-to-end test for accessing stage help

---
+++ test/features/access_stage_help_test.exs
@@ -0,0 +1,21 @@
+defmodule AccessStageHelpTest do
+  use RemoteRetro.IntegrationCase, async: false
+
+  import ShorterMaps
+
+  @tag [
+    retro_stage: "action-items",
+  ]
+  test "Distributing action items via email", ~M{retro, session} do
+    retro_path = "/retros/" <> retro.id
+    visit(session, retro_path)
+
+    trigger_help(session)
+
+    assert_has(session, Query.css(".ui.modal", text: "The skinny on Action-Item Generation"))
+  end
+
+  defp trigger_help(session) do
+    click(session, Query.css("i.question"))
+  end
+end
