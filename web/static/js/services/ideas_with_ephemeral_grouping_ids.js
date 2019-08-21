f8db9187f7420ced0068086d127d082285861428
Travis Vander Hoop <vanderhoop@me.com>
2019-08-21 16:50:13 -0600

    Refactor & performance two'fer: consolidate ephemeralGroupingCandidate reduce call with the keyBy
      - rather than iterate over the ideas collection multiple times, we iterate once in a reduce call

--- web/static/js/services/ideas_with_ephemeral_grouping_ids.js
+++ web/static/js/services/ideas_with_ephemeral_grouping_ids.js
@@ -1,27 +1,23 @@
-import keyBy from "lodash/keyBy"
-import cloneDeep from "lodash/cloneDeep"
 import Collisions from "./collisions"
 
 export default {
   buildFrom: ideas => {
-    const ideaIdsToEphemeralGroupingIdsMap = ideas.reduce((accumulator, idea, index) => {
-      accumulator[idea.id] = index + 1
-      return accumulator
-    }, {})
-
     const collisions = Collisions.identifyAllIdeaCollisionsSortedByIdAscending(ideas)
     const collisionsDeduped = Collisions.merge(collisions)
 
-    // return new objects to ensure stale ephemeral grouping ids get flushed
-    const ideasById = cloneDeep(keyBy(ideas, "id"))
+    const ideasByIdWithEphemeralGroupingIdCandidate = ideas.reduce((accumulator, idea, index) => {
+      accumulator[idea.id] = { ...idea, ephemeralGroupingIdCandidate: index + 1 }
+      return accumulator
+    }, {})
 
     for (const [groupLeaderId, collisionsForIdea] of collisionsDeduped) {
       collisionsForIdea.forEach(relatedIdeaId => {
-        const ephemeralGroupingId = ideaIdsToEphemeralGroupingIdsMap[groupLeaderId]
-        ideasById[relatedIdeaId].ephemeralGroupingId = ephemeralGroupingId
+        const groupLeaderEphemeralGroupingId = ideasByIdWithEphemeralGroupingIdCandidate[groupLeaderId].ephemeralGroupingIdCandidate
+        ideasByIdWithEphemeralGroupingIdCandidate[relatedIdeaId].ephemeralGroupingId = groupLeaderEphemeralGroupingId
       })
     }
 
-    return Object.values(ideasById)
+    return Object.values(ideasByIdWithEphemeralGroupingIdCandidate)
+      .map(({ ephemeralGroupingIdCandidate, ...rest }) => rest)
   },
 }
