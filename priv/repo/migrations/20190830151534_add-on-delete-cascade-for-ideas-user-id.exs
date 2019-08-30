774cf829b1755748bd038286a4a1b564e447b0f5
Travis Vander Hoop <vanderhoop@me.com>
2019-08-30 09:21:51 -0600

    Add ON DELETE CASCADE to ideas authored by a user

---
+++ priv/repo/migrations/20190830151534_add-on-delete-cascade-for-ideas-user-id.exs
@@ -0,0 +1,23 @@
+defmodule :"Elixir.RemoteRetro.Repo.Migrations.Add-on-delete-cascade-for-ideas-user-id" do
+  use Ecto.Migration
+
+  def up do
+    drop constraint(:ideas, "ideas_user_id_fkey")
+    alter table("ideas") do
+      modify :user_id, references(:users, [
+        column: :id,
+        on_delete: :delete_all
+      ])
+    end
+  end
+
+  def down do
+    drop constraint(:ideas, "ideas_user_id_fkey")
+    alter table("ideas") do
+      modify :user_id, references(:users, [
+        column: :id,
+        on_delete: :nothing
+      ])
+    end
+  end
+end
