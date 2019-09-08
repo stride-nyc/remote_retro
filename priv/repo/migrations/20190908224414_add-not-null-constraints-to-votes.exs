daaf64285b0807bffb2102be0eafc79e3e1fe04f
Travis Vander Hoop <vanderhoop@me.com>
2019-09-08 19:11:48 -0400

    Add not null constraints to votes.{user_id, idea_id}

---
+++ priv/repo/migrations/20190908224414_add-not-null-constraints-to-votes.exs
@@ -0,0 +1,49 @@
+defmodule :"Elixir.RemoteRetro.Repo.Migrations.Add-not-null-constraints-to-votes" do
+  use Ecto.Migration
+
+  def up do
+    drop_if_exists index(:votes, [:user_id])
+    drop_if_exists index(:votes, [:idea_id])
+
+    drop constraint(:votes, "votes_user_id_fkey")
+    drop constraint(:votes, "votes_idea_id_fkey")
+
+    alter table(:votes) do
+      modify(:user_id, references(:users, [
+        column: :id,
+        on_delete: :delete_all,
+      ]), null: false)
+
+      modify(:idea_id, references(:ideas, [
+        column: :id,
+        on_delete: :delete_all,
+      ]), null: false)
+    end
+
+    create_if_not_exists index(:votes, [:user_id])
+    create_if_not_exists index(:votes, [:idea_id])
+  end
+
+  def down do
+    drop_if_exists index(:votes, [:user_id])
+    drop_if_exists index(:votes, [:idea_id])
+
+    drop constraint(:votes, "votes_user_id_fkey")
+    drop constraint(:votes, "votes_idea_id_fkey")
+
+    alter table(:votes) do
+      modify(:user_id, references(:users, [
+        column: :id,
+        on_delete: :delete_all,
+      ]), null: true)
+
+      modify(:idea_id, references(:ideas, [
+        column: :id,
+        on_delete: :delete_all,
+      ]), null: true)
+    end
+
+    create_if_not_exists index(:votes, [:user_id])
+    create_if_not_exists index(:votes, [:idea_id])
+  end
+end
