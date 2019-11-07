22e03e15a64f391580f7e9a40d93b4121abfa8be
Travis Vander Hoop <vanderhoop@me.com>
2019-11-06 23:10:40 -0600

    Remove temporary production logging used to identify problematic data

--- lib/remote_retro_web/services/user_management.ex
+++ lib/remote_retro_web/services/user_management.ex
@@ -2,10 +2,6 @@ defmodule RemoteRetroWeb.UserManagement do
   alias RemoteRetro.{User, Mailer, Emails}
 
   def handle_google_oauth(oauth_info) do
-    if Application.get_env(:remote_retro, :env) !== :test do
-      IO.inspect oauth_info
-    end
-
     case User.upsert_record_from(oauth_info: oauth_info) do
       {:ok, user, :inserted} ->
         Emails.welcome_email(user) |> Mailer.deliver_now()
