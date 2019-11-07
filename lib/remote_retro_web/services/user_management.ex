a352ee7cf08a3fb98af12d5b5f5e243fbc3a2632
Travis Vander Hoop <vanderhoop@me.com>
2019-11-06 18:42:13 -0600

    Logging: log oauth_info

--- lib/remote_retro_web/services/user_management.ex
+++ lib/remote_retro_web/services/user_management.ex
@@ -2,6 +2,8 @@ defmodule RemoteRetroWeb.UserManagement do
   alias RemoteRetro.{User, Mailer, Emails}
 
   def handle_google_oauth(oauth_info) do
+    IO.inspect oauth_info
+
     case User.upsert_record_from(oauth_info: oauth_info) do
       {:ok, user, :inserted} ->
         Emails.welcome_email(user) |> Mailer.deliver_now()
