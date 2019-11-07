c9424d179475c427fb16f3525b4fe3e629af51a7
Travis Vander Hoop <vanderhoop@me.com>
2019-11-06 18:59:45 -0600

    Scope inspection of oauth_info to non-test environments

--- lib/remote_retro_web/services/user_management.ex
+++ lib/remote_retro_web/services/user_management.ex
@@ -2,7 +2,9 @@ defmodule RemoteRetroWeb.UserManagement do
   alias RemoteRetro.{User, Mailer, Emails}
 
   def handle_google_oauth(oauth_info) do
-    IO.inspect oauth_info
+    if Application.get_env(:remote_retro, :env) !== :test do
+      IO.inspect oauth_info
+    end
 
     case User.upsert_record_from(oauth_info: oauth_info) do
       {:ok, user, :inserted} ->
