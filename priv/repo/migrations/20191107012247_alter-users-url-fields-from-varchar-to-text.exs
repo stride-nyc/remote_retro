dfd31c3ad8f217f349b5376ca41fc592451afb8a
Travis Vander Hoop <vanderhoop@me.com>
2019-11-06 19:27:05 -0600

    Database: alter users.picture column from varchar to text type
      - see https://stackoverflow.com/questions/219569/best-database-field-type-for-a-url

---
+++ priv/repo/migrations/20191107012247_alter-users-url-fields-from-varchar-to-text.exs
@@ -0,0 +1,15 @@
+defmodule :"Elixir.RemoteRetro.Repo.Migrations.Alter-users-url-fields-from-varchar-to-text" do
+  use Ecto.Migration
+
+  def up do
+    alter table(:users) do
+      modify :picture, :text
+    end
+  end
+
+  def down do
+    alter table(:users) do
+      modify :picture, :string
+    end
+  end
+end
