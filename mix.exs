fc0d123395201a754c5bc1ea604ae8d5e69a7e0e
Travis Vander Hoop <vanderhoop@me.com>
2019-11-09 10:29:59 -0600

    Deps: bump phoenix 1.4.9 -> 1.4.11
      - brings in fix for channel client (javascript) memory leaks
        - https://github.com/phoenixframework/phoenix/blob/v1.4/CHANGELOG.md#1411-2019-11-09

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule RemoteRetro.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.9"},
+      {:phoenix, "~> 1.4.11"},
       {:phoenix_pubsub, "~> 1.1.2"},
       {:phoenix_ecto, "~> 4.0.0"},
       {:ecto_sql, "~> 3.1.6"},
