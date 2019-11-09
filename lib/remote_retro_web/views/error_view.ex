927d7ea2bf7700df88dba035b684f82800d7ea6b
Travis Vander Hoop <vanderhoop@me.com>
2019-11-08 21:38:52 -0600

    Add custom 404 & 5xx error views
      - per https://hexdocs.pm/phoenix/views.html#the-errorview

--- lib/remote_retro_web/views/error_view.ex
+++ lib/remote_retro_web/views/error_view.ex
@@ -1,14 +1,6 @@
 defmodule RemoteRetroWeb.ErrorView do
   use RemoteRetroWeb, :view
 
-  def render("404.html", _assigns) do
-    "Page not found"
-  end
-
-  def render("500.html", _assigns) do
-    "Internal server error"
-  end
-
   # In case no render clause matches or no
   # template is found, let's render it as 500
   def template_not_found(_template, assigns) do
--- test/views/error_view_test.exs
+++ test/views/error_view_test.exs
@@ -1,18 +1,20 @@
 defmodule RemoteRetro.ErrorViewTest do
   use RemoteRetroWeb.ConnCase, async: true
 
+  alias RemoteRetroWeb.ErrorView
+
   # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
 
-  test "renders 404.html" do
-    assert render_to_string(RemoteRetroWeb.ErrorView, "404.html", []) == "Page not found"
+  test "requests to a page that doesn't exist surfaces a 404" do
+    assert render_to_string(ErrorView, "404.html", []) =~ "Page not found"
   end
 
   test "render 500.html" do
-    assert render_to_string(RemoteRetroWeb.ErrorView, "500.html", []) == "Internal server error"
+    assert render_to_string(ErrorView, "500.html", []) =~ "Internal server error"
   end
 
-  test "render any other" do
-    assert render_to_string(RemoteRetroWeb.ErrorView, "505.html", []) == "Internal server error"
+  test "render any other 5xx error as a generic server error" do
+    assert render_to_string(ErrorView, "505.html", []) =~ "Internal server error"
   end
 end
