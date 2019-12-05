4137854e1338d92d0a23182124078e84b56485e3
Travis Vander Hoop <vanderhoop@me.com>
2019-12-04 18:44:48 -0700

    Copy: update stage change info for group naming stage

--- web/static/js/components/stage_change_info_group_naming.jsx
+++ web/static/js/components/stage_change_info_group_naming.jsx
@@ -5,7 +5,17 @@ export default () => (
     The skinny on Group Naming:
     <div className="ui basic segment">
       <ul className="ui list">
-        <li>Arrive at a sensible name for the various groups of ideas</li>
+        <li>
+          Help the facilitator arrive at a sensible name/label for each group of ideas
+        </li>
+        <li>
+          Don't feel that you need to name groups that contain a single idea.
+          (Their name is implicit!)
+        </li>
+        <li>
+          Don't feel that you need to get the name perfect!
+          Perfectionism has diminishing returns in this stage.
+        </li>
       </ul>
     </div>
   </React.Fragment>
