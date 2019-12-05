4b15c713929f0cb572a4d892ee22ecfa2fafae63
Travis Vander Hoop <vanderhoop@me.com>
2019-12-04 18:44:46 -0700

    Copy: update stage change info for idea grouping stage

--- web/static/js/components/stage_change_info_grouping.jsx
+++ web/static/js/components/stage_change_info_grouping.jsx
@@ -5,7 +5,19 @@ export default () => (
     The skinny on Idea Grouping:
     <div className="ui basic segment">
       <ul className="ui list">
-        <li>Group related items by bringing them into contact with one another</li>
+        <li>
+          <strong>Do</strong> bring ideas with common themes into contact.
+        </li>
+        <li>
+          <strong>Do</strong> drag highly disparate ideas far apart.
+        </li>
+        <li>
+          <strong>Don't</strong> feel that solo ideas are off-limits. They're not.
+        </li>
+        <li>
+          <strong>Don't</strong> speak.
+          If there's a disagreement, attempt to settle it without speaking.
+        </li>
       </ul>
     </div>
   </React.Fragment>
