4ccbd162d1d4d5cf4d6cdd12365848d597f099f8
Anurup Joseph <ajoseph@stridenyc.com>
2020-01-30 17:44:24 -0500

    Checking in work in progress.

--- web/static/js/components/group_label_input.jsx
+++ web/static/js/components/group_label_input.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { Component } from "react"
 import * as AppPropTypes from "../prop_types"
 
 import styles from "./css_modules/group_label_input.css"
@@ -7,23 +7,34 @@ import styles from "./css_modules/group_label_input.css"
 // exceed the 1/4th column width on desktop, even with the widest possible characters
 const MAX_LENGTH_OF_GROUP_NAME = 20
 
-const GroupLabelInput = ({ groupWithAssociatedIdeasAndVotes, actions }) => (
-  <div className="ui fluid input">
-    <input
-      type="text"
-      className={styles.textInput}
-      placeholder="Add a group label"
-      defaultValue={groupWithAssociatedIdeasAndVotes.label}
-      maxLength={MAX_LENGTH_OF_GROUP_NAME}
-      onBlur={e => {
-        actions.submitGroupLabelChanges(groupWithAssociatedIdeasAndVotes, e.target.value)
-      }}
-    />
-    <div className="instruction">
-      <span className="keyboard-key">tab</span> to submit
-    </div>
-  </div>
-)
+class GroupLabelInput extends Component {
+  componentDidMount() {
+    console.log("DELETE ME; only added to enable the pre-commit hook to permit checkin")
+  }
+
+  render() {
+    const { groupWithAssociatedIdeasAndVotes, actions } = this.props
+
+    return (
+      <div className="ui fluid input">
+        <input
+          type="text"
+          className={styles.textInput}
+          placeholder="Add a group label"
+          defaultValue={groupWithAssociatedIdeasAndVotes.label}
+          maxLength={MAX_LENGTH_OF_GROUP_NAME}
+          onBlur={e => {
+            actions.submitGroupLabelChanges(groupWithAssociatedIdeasAndVotes, e.target.value)
+          }}
+        />
+        <div className="instruction">
+          <span className="keyboard-key">tab</span> to submit
+        </div>
+        <i className="check icon" />
+      </div>
+    )
+  }
+}
 
 GroupLabelInput.propTypes = {
   actions: AppPropTypes.actions.isRequired,
