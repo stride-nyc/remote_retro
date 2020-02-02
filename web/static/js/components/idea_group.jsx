7eb9a3c1fd6b5bd878d9dbe0bd76a5c4254dad84
Travis Vander Hoop <vanderhoop@me.com>
2020-02-02 09:07:37 -0700

    Refactor: extract inline prop derivations in <IdeaGroup /> out into semantically named vars

--- web/static/js/components/idea_group.jsx
+++ web/static/js/components/idea_group.jsx
@@ -10,29 +10,34 @@ import STAGES from "../configs/stages"
 
 const { LABELING_PLUS_VOTING } = STAGES
 
-const IdeaGroup = ({ groupWithAssociatedIdeasAndVotes, currentUser, actions, stage }) => (
-  <div className={`idea-group ${styles.wrapper}`}>
-    <GroupLabelContainer
-      actions={actions}
-      currentUser={currentUser}
-      groupWithAssociatedIdeasAndVotes={groupWithAssociatedIdeasAndVotes}
-    />
+const IdeaGroup = ({ groupWithAssociatedIdeasAndVotes, currentUser, actions, stage }) => {
+  const ideaToCastVoteFor = groupWithAssociatedIdeasAndVotes.ideas[0]
+  const isVotingStage = stage === LABELING_PLUS_VOTING
 
-    <VotingInterface
-      actions={actions}
-      currentUser={currentUser}
-      isVotingStage={stage === LABELING_PLUS_VOTING}
-      ideaToCastVoteFor={groupWithAssociatedIdeasAndVotes.ideas[0]}
-      votesForEntity={groupWithAssociatedIdeasAndVotes.votes}
-    />
+  return (
+    <div className={`idea-group ${styles.wrapper}`}>
+      <GroupLabelContainer
+        actions={actions}
+        currentUser={currentUser}
+        groupWithAssociatedIdeasAndVotes={groupWithAssociatedIdeasAndVotes}
+      />
 
-    <ul className={styles.list}>
-      {groupWithAssociatedIdeasAndVotes.ideas.map(idea => {
-        return <li key={idea.id} className={ideaStyles.index}>{idea.body}</li>
-      })}
-    </ul>
-  </div>
-)
+      <VotingInterface
+        actions={actions}
+        currentUser={currentUser}
+        isVotingStage={isVotingStage}
+        ideaToCastVoteFor={ideaToCastVoteFor}
+        votesForEntity={groupWithAssociatedIdeasAndVotes.votes}
+      />
+
+      <ul className={styles.list}>
+        {groupWithAssociatedIdeasAndVotes.ideas.map(idea => {
+          return <li key={idea.id} className={ideaStyles.index}>{idea.body}</li>
+        })}
+      </ul>
+    </div>
+  )
+}
 
 IdeaGroup.propTypes = {
   actions: AppPropTypes.actions.isRequired,
