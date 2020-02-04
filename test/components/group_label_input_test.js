700e1431111c2d8184fe10bc2c6f3d36070288d8
jennmiller <jmiller@stridenyc.com>
2020-02-04 16:39:29 -0500

    Add spec for appending checkmark

--- test/components/group_label_input_test.js
+++ test/components/group_label_input_test.js
@@ -5,9 +5,11 @@ import sinon from "sinon"
 import GroupLabelInput from "../../web/static/js/components/group_label_input"
 
 describe("GroupLabelInput component", () => {
+  let wrapper
+  let groupWithAssociatedIdeasAndVotes
+
   describe("upon blurring the input field", () => {
     let submitGroupLabelChangesSpy
-    let groupWithAssociatedIdeasAndVotes
 
     beforeEach(() => {
       groupWithAssociatedIdeasAndVotes = {
@@ -18,7 +20,7 @@ describe("GroupLabelInput component", () => {
       }
 
       submitGroupLabelChangesSpy = sinon.spy()
-      const wrapper = shallow(
+      wrapper = shallow(
         <GroupLabelInput
           groupWithAssociatedIdeasAndVotes={groupWithAssociatedIdeasAndVotes}
           actions={{ submitGroupLabelChanges: submitGroupLabelChangesSpy }}
@@ -33,6 +35,33 @@ describe("GroupLabelInput component", () => {
       expect(
         submitGroupLabelChangesSpy
       ).to.have.been.calledWith(groupWithAssociatedIdeasAndVotes, "Turtles")
+    })
+  })
+
+  describe("upon the group label changing", () => {
+    it("adds a checkmark next to the label", () => {
+      groupWithAssociatedIdeasAndVotes = {
+        id: 777,
+        label: "some previous label",
+        ideas: [],
+        votes: [],
+      }
+      wrapper = shallow(
+        <GroupLabelInput
+          groupWithAssociatedIdeasAndVotes={groupWithAssociatedIdeasAndVotes}
+          actions={{}}
+        />
+      )
+      const newGroupWithAssociatedIdeasAndVotes = {
+        id: 777,
+        label: "a better label",
+        ideas: [],
+        votes: [],
+      }
+
+      expect(wrapper.find(".check")).to.have.lengthOf(0)
+      wrapper.setProps({ groupWithAssociatedIdeasAndVotes: newGroupWithAssociatedIdeasAndVotes })
+      expect(wrapper.find(".check")).to.have.lengthOf(1)
     })
   })
 })
