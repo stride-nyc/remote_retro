e8d1ea62ac864db99bbd4476c4a49da89f57f02b
Travis Vander Hoop <vanderhoop@me.com>
2020-02-10 14:57:37 -0700

    Ensure closed stage maintains GroupsContainer when feature flag is engaged

--- web/static/js/configs/shared_stage_configs.js
+++ web/static/js/configs/shared_stage_configs.js
@@ -139,7 +139,7 @@ export default {
       headerText: "Retro is Closed!",
       BodyComponent: StageChangeInfoClosed,
     },
-    uiComponent: IdeationInterface,
+    uiComponent: localStorage.getItem("groupingDev") ? GroupsContainer : IdeationInterface,
     progressionButton: null,
   },
 }
