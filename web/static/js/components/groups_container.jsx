41657f607102a2848c42f73f8b365d1fa240c56d
Travis Vander Hoop <vanderhoop@me.com>
2020-02-11 10:14:09 -0700

    Enhancement: add shadow to overflowed group list

--- web/static/js/components/groups_container.jsx
+++ web/static/js/components/groups_container.jsx
@@ -1,8 +1,10 @@
-import React from "react"
+import React, { Component } from "react"
 import { connect } from "react-redux"
 import orderBy from "lodash/orderBy"
 import PropTypes from "prop-types"
 import FlipMove from "react-flip-move"
+import classNames from "classnames"
+import { OverflowDetector } from "react-overflow"
 
 import { selectors } from "../redux/index"
 
@@ -24,57 +26,76 @@ const sortGroups = (groupsWithAssociatedIdeasAndVotes, isLabelingPlusVotingStage
     : orderBy(groupsWithAssociatedIdeasAndVotes, ["votes.length", "id"], ["desc", "asc"])
 }
 
-export const GroupsContainer = props => {
-  const {
-    groupsWithAssociatedIdeasAndVotes,
-    currentUser,
-    actions,
-    stage,
-    currentUserHasExhaustedVotes,
-  } = props
+export class GroupsContainer extends Component {
+  state = {
+    isGroupsListOverflowed: false,
+  }
 
-  const isLabelingPlusVotingStage = stage === LABELING_PLUS_VOTING
-  const groupsSorted = sortGroups(groupsWithAssociatedIdeasAndVotes, isLabelingPlusVotingStage)
+  handleOverflowChange = isGroupsListOverflowed => {
+    this.setState({ isGroupsListOverflowed })
+  }
 
-  return (
-    <div className={styles.wrapper}>
-      <div className={styles.flexContainerForGroupsAndOptionallyActionItems}>
-        <FlipMove
-          delay={500}
-          duration={750}
-          staggerDelayBy={100}
-          easing="ease"
-          enterAnimation="none"
-          leaveAnimation="none"
-          className={styles.groupsWrapper}
-        >
-          {groupsSorted.map(groupWithAssociatedIdeasAndVotes => (
-            <IdeaGroup
-              actions={actions}
+  render() {
+    const { isGroupsListOverflowed } = this.state
+    const {
+      groupsWithAssociatedIdeasAndVotes,
+      currentUser,
+      actions,
+      stage,
+      currentUserHasExhaustedVotes,
+    } = this.props
+
+    const isLabelingPlusVotingStage = stage === LABELING_PLUS_VOTING
+    const groupsSorted = sortGroups(groupsWithAssociatedIdeasAndVotes, isLabelingPlusVotingStage)
+    const groupsListClasses = classNames(styles.groupsWrapper, {
+      overflowed: isGroupsListOverflowed,
+    })
+
+    return (
+      <div className={styles.wrapper}>
+        <div className={styles.flexContainerForGroupsAndOptionallyActionItems}>
+          <OverflowDetector
+            onOverflowChange={this.handleOverflowChange}
+            className={groupsListClasses}
+          >
+            <FlipMove
+              delay={500}
+              duration={750}
+              staggerDelayBy={100}
+              easing="ease"
+              enterAnimation="none"
+              leaveAnimation="none"
+              typeName={null}
+            >
+              {groupsSorted.map(groupWithAssociatedIdeasAndVotes => (
+                <IdeaGroup
+                  actions={actions}
+                  currentUser={currentUser}
+                  currentUserHasExhaustedVotes={currentUserHasExhaustedVotes}
+                  key={groupWithAssociatedIdeasAndVotes.id}
+                  stage={stage}
+                  groupWithAssociatedIdeasAndVotes={groupWithAssociatedIdeasAndVotes}
+                />
+              ))}
+            </FlipMove>
+          </OverflowDetector>
+
+          {!isLabelingPlusVotingStage && (
+            <CategoryColumn
+              category="action-item"
+              categoryDisplayStringOverride="Action Items"
               currentUser={currentUser}
-              currentUserHasExhaustedVotes={currentUserHasExhaustedVotes}
-              key={groupWithAssociatedIdeasAndVotes.id}
               stage={stage}
-              groupWithAssociatedIdeasAndVotes={groupWithAssociatedIdeasAndVotes}
             />
-          ))}
-        </FlipMove>
+          )}
+        </div>
 
-        {!isLabelingPlusVotingStage && (
-          <CategoryColumn
-            category="action-item"
-            categoryDisplayStringOverride="Action Items"
-            currentUser={currentUser}
-            stage={stage}
-          />
-        )}
+        <UserList wrap={false} />
+
+        <LowerThird {...this.props} />
       </div>
-
-      <UserList wrap={false} />
-
-      <LowerThird {...props} />
-    </div>
-  )
+    )
+  }
 }
 
 GroupsContainer.propTypes = {
