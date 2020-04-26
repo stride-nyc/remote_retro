0d46fffc7695825b919bb388b539ff013d2c7c7e
Travis Vander Hoop <vanderhoop@me.com>
2020-04-26 11:14:05 -0600

    Add request logger to Phoenix.LiveDashboard

--- lib/remote_retro_web/endpoint.ex
+++ lib/remote_retro_web/endpoint.ex
@@ -28,6 +28,11 @@ defmodule RemoteRetroWeb.Endpoint do
     plug(Phoenix.CodeReloader)
   end
 
+  plug(
+    Phoenix.LiveDashboard.RequestLogger,
+    param_key: "request_logger",
+    cookie_key: "request_logger"
+  )
   plug(Plug.RequestId)
   plug(Plug.Logger)
 
