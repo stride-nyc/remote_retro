8e01de37786ae2e4f46ad94832c627a6e562c653
Travis Vander Hoop <vanderhoop@me.com>
2020-05-08 18:21:39 -0600

    Bugfix: avoid edge case where grouping dimmer surfaces without inner prompt to change device orientation in grouping stage
      - there were cases where non-devices were technically in portrait orientation, so we were surfacing the dimmer, but we weren't triggering the device/non-device queries to surface the appropriate message *within* the dimmer, so it just obfuscated content without surfacing the appropriate message
      - the fix is to rely solely on CSS media queries, combining max-device-width and orientation, for conditionally surfacing prompt to shift from landscape to portrait (for devices)

--- web/static/js/components/grouping_stage.jsx
+++ web/static/js/components/grouping_stage.jsx
@@ -9,10 +9,8 @@ import IdeasWithEphemeralGroupingIds from "../services/ideas_with_ephemeral_grou
 
 import styles from "./css_modules/grouping_stage.css"
 
-const PORTRAIT = "portrait"
-
 const GroupingStage = props => {
-  const { ideas, actions, browser, userOptions } = props
+  const { ideas, actions, userOptions } = props
 
   const ideasWithEphemeralGroupingIds = IdeasWithEphemeralGroupingIds.buildFrom(ideas)
 
@@ -26,23 +24,25 @@ const GroupingStage = props => {
 
       <LowerThird {...props} />
 
-      {browser.orientation === PORTRAIT && (
-        <div className="ui dimmer visible transition visible active">
-          <div className="content">
-            <h3 className="ui inverted icon header device">
-              Rotate your device!
-              <p className="sub header">You're in portrait mode; this stage requires landscape!</p>
-              <i className="mobile alternate icon" />
-            </h3>
-
-            <h3 className="ui inverted icon header non-device">
-              <i className="expand arrows alternate icon" />
-              Expand this window!
-              <p className="sub header">This stage requires a wide viewport!</p>
-            </h3>
-          </div>
+      <div className="ui dimmer visible transition active device">
+        <div className="content">
+          <h3 className="ui inverted icon header device">
+            Rotate your device!
+            <p className="sub header">You're in portrait mode; this stage requires landscape!</p>
+            <i className="mobile alternate icon" />
+          </h3>
         </div>
-      )}
+      </div>
+
+      <div className="ui dimmer visible transition active non-device">
+        <div className="content">
+          <h3 className="ui inverted icon header">
+            <i className="expand arrows alternate icon" />
+            Expand this window!
+            <p className="sub header">This stage requires a wide viewport!</p>
+          </h3>
+        </div>
+      </div>
     </div>
   )
 }
@@ -50,7 +50,6 @@ const GroupingStage = props => {
 GroupingStage.propTypes = {
   ideas: AppPropTypes.ideas.isRequired,
   actions: PropTypes.object.isRequired,
-  browser: PropTypes.object.isRequired,
   userOptions: AppPropTypes.userOptions.isRequired,
 }
 
