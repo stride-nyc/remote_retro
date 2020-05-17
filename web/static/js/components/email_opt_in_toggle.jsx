ba9730f06874ceb0a148c56095fc84b9952796c3
Travis Vander Hoop <vanderhoop@me.com>
2020-05-17 12:48:23 -0600

    Remove unnecessary wrapping div

--- web/static/js/components/email_opt_in_toggle.jsx
+++ web/static/js/components/email_opt_in_toggle.jsx
@@ -6,25 +6,23 @@ const EmailOptInToggle = props => {
   const { actions, currentUser } = props
 
   return (
-    <div className="ui secondary compact segment thirteen wide mobile eight wide tablet four wide computer column">
-      <div className={styles.wrapper}>
-        <p>
-          Would you like to receive occasional emails from RemoteRetro
-          and Stride Consulting? You can opt out any time, per our <a href="/privacy">privacy policy</a>.
-        </p>
-        <button
-          className="ui basic compact button"
-          type="button"
-          onClick={() => {
-            actions.updateUserAsync(currentUser.id, { email_opt_in: !currentUser.email_opt_in })
-          }}
-        >
-          <div className="ui toggle checkbox">
-            <input type="checkbox" name="public" checked={currentUser.email_opt_in} readOnly />
-            <label>Sign me up!</label>
-          </div>
-        </button>
-      </div>
+    <div className="email-opt-in-toggle ui secondary compact segment thirteen wide mobile eight wide tablet four wide computer column">
+      <p>
+        Would you like to receive occasional emails from RemoteRetro
+        and Stride Consulting? You can opt out any time, per our <a href="/privacy">privacy policy</a>.
+      </p>
+      <button
+        className="ui basic fluid compact button"
+        type="button"
+        onClick={() => {
+          actions.updateUserAsync(currentUser.id, { email_opt_in: !currentUser.email_opt_in })
+        }}
+      >
+        <div className="ui toggle checkbox">
+          <input type="checkbox" name="public" checked={currentUser.email_opt_in} readOnly />
+          <label>Sign me up!</label>
+        </div>
+      </button>
     </div>
   )
 }
