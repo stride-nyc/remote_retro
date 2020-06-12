bd0470460463c91626636bebdf8c59ff66d3361d
Travis Vander Hoop <vanderhoop@me.com>
2020-06-11 18:47:41 -0600

    remove bolding from 'blind' in 'voting is blind' note

--- web/static/js/components/stage_change_info_voting.jsx
+++ web/static/js/components/stage_change_info_voting.jsx
@@ -8,7 +8,7 @@ export default () => (
         <li>Work as a team to arrive at a sensible label for each group <span className="optional">(optional)</span></li>
         <li>Apply votes to the items you feel are <strong>most important</strong> to discuss.
           <p className="list-item-note">
-            <strong>Note: </strong>voting is <strong>blind</strong>.
+            <strong>Note: </strong>voting is blind.
             Totals will be revealed when the facilitator advances the retro.
           </p>
         </li>
