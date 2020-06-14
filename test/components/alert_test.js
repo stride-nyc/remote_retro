cbd34490c815b12d13a035324a0423fd5955142d
Travis Vander Hoop <vanderhoop@me.com>
2020-06-13 20:25:28 -0600

    Add proper nesting to alert.jsx test

--- test/components/alert_test.js
+++ test/components/alert_test.js
@@ -16,39 +16,41 @@ describe("Alert component", () => {
 
   let actions = {}
 
-  beforeEach(() => {
-    wrapper = mountWithConnectedSubcomponents(<Alert config={alertConfig} actions={actions} />)
-    portalToModalContent = wrapper.find(Modal).instance().portal
-    modalBody = portalToModalContent.content
-  })
-
-  it("renders the headerText in a header text component", () => {
-    expect(modalBody.querySelector(".header").textContent).to.match(/some ole header text/i)
-  })
-
-  it("renders the headerText in a header text component", () => {
-    expect(
-      modalBody.querySelector(".content").textContent
-    ).to.match(/some completely different body text/i)
-  })
-
-  it("renders the button autofocused", () => {
-    const button = modalBody.querySelector("button")
-    expect(button).to.deep.equal(document.activeElement)
-  })
-
-  describe("clicking the button", () => {
+  describe("when passed an alert config", () => {
     beforeEach(() => {
-      actions = { clearAlert: sinon.spy() }
-
       wrapper = mountWithConnectedSubcomponents(<Alert config={alertConfig} actions={actions} />)
       portalToModalContent = wrapper.find(Modal).instance().portal
       modalBody = portalToModalContent.content
-      modalBody.querySelector("button").click()
     })
 
-    it("invokes the clearAlert action", () => {
-      expect(actions.clearAlert).called
+    it("renders the headerText in a header text component", () => {
+      expect(modalBody.querySelector(".header").textContent).to.match(/some ole header text/i)
+    })
+
+    it("renders the headerText in a header text component", () => {
+      expect(
+        modalBody.querySelector(".content").textContent
+      ).to.match(/some completely different body text/i)
+    })
+
+    it("renders the button autofocused", () => {
+      const button = modalBody.querySelector("button")
+      expect(button).to.deep.equal(document.activeElement)
+    })
+
+    describe("clicking the button", () => {
+      beforeEach(() => {
+        actions = { clearAlert: sinon.spy() }
+
+        wrapper = mountWithConnectedSubcomponents(<Alert config={alertConfig} actions={actions} />)
+        portalToModalContent = wrapper.find(Modal).instance().portal
+        modalBody = portalToModalContent.content
+        modalBody.querySelector("button").click()
+      })
+
+      it("invokes the clearAlert action", () => {
+        expect(actions.clearAlert).called
+      })
     })
   })
 })
