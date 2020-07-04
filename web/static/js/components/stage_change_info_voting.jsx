dff656a69714bc9c13c02960b80fd9668d147a6b
Travis Vander Hoop <vanderhoop@me.com>
2020-07-04 11:29:18 -0600

    Remove unnecessary whitespace from 'Note:' bullet point

--- web/static/js/components/stage_change_info_voting.jsx
+++ web/static/js/components/stage_change_info_voting.jsx
@@ -7,8 +7,7 @@ export default () => (
       <ul className="ui list">
         <li>Work as a team to arrive at a sensible label for each group <span className="optional">(optional)</span></li>
         <li>Apply votes to the items you feel are <strong>most important</strong> to discuss.
-          <p className="list-item-note">
-            <strong>Note: </strong>voting is blind.
+          <p className="list-item-note"><strong>Note: </strong>voting is blind.
             Totals will be revealed when the facilitator advances the retro.
           </p>
         </li>
