cfd04d52df1cc16f694e90be3e500135ae41ee6b
Travis Vander Hoop <vanderhoop@me.com>
2020-07-06 09:43:00 -0600

    Update 'Prime Directive' stage change guidance

--- web/static/js/components/stage_change_info_prime_directive.jsx
+++ web/static/js/components/stage_change_info_prime_directive.jsx
@@ -1,8 +1,19 @@
 import React from "react"
 
 export default () => (
-  <p>
-    The Prime Directive is used to frame the retrospective, such that the time
-    spent is as constructive as possible. Read it to yourself! Internalize it!
-  </p>
+  <React.Fragment>
+    <a href="https://retrospectivewiki.org/index.php?title=The_Prime_Directive" target="_blank">Norm Kerth's Prime Directive</a> sets the stage for the retrospective, such that the time spent is as constructive as possible.
+    <br />
+    <br />
+    <div className="ui content">
+      <p><strong>Guidance:</strong></p>
+      <ul className="ui list">
+        <li>Solicit a volunteer to read the Prime Directive aloud.</li>
+        <li>Ask each member of the team if they can agree to the Prime Directive.
+          <p className="list-item-note"><strong>Note: </strong>If someone earnestly <em>cannot</em> agree, there's likely a deep trust issue on the team that should be addressed with a manager.</p>
+        </li>
+        <li>Once the team has agreed to the Prime Directive, advance to Idea Generation.</li>
+      </ul>
+    </div>
+  </React.Fragment>
 )
--- web/static/js/components/stage_change_info_voting.jsx
+++ web/static/js/components/stage_change_info_voting.jsx
@@ -5,7 +5,7 @@ export default () => (
     Guidance:
     <div className="ui basic segment">
       <ul className="ui list">
-        <li>Work as a team to arrive at a sensible label for each group <span className="optional">(optional)</span></li>
+        <li>Work as a team to arrive at a sensible label for each group. <span className="optional">(optional)</span></li>
         <li>Apply votes to the items you feel are <strong>most important</strong> to discuss.
           <p className="list-item-note"><strong>Note: </strong>voting is blind.
             Totals will be revealed when the facilitator advances the retro.
