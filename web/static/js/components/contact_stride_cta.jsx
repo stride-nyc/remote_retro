7c0324d0cd99e8e5203a0d3046d7733f2926ad01
Travis Vander Hoop <vanderhoop@me.com>
2020-07-14 11:42:21 -0600

    Add stride logo

--- web/static/js/components/contact_stride_cta.jsx
+++ web/static/js/components/contact_stride_cta.jsx
@@ -5,10 +5,14 @@ import styles from "./css_modules/contact_stride_cta.css"
 const ContactStrideCTA = () => {
   return (
     <section className={styles.wrapper}>
+      <img src="/images/stride_logo.png" alt="" />
       <h3 className="ui header">
-        This Retrospective was brought to you by Stride Consulting.
+        This retrospective was brought to you by Stride Consulting.
       </h3>
 
+      <p>Got software problems?</p>
+
+      <button className="ui tiny green button">Email Debbie!</button>
     </section>
   )
 }
