9bcf695534a8eefdb6dfe9df3b01cdce453f89a3
Travis Vander Hoop <vanderhoop@me.com>
2020-11-15 09:09:13 -0700

    Remove unused indexes surfaced by phoenix_live_dashboard

---
+++ priv/repo/migrations/20201115160132_remove_unused_indexes.exs
@@ -0,0 +1,10 @@
+defmodule RemoteRetro.Repo.Migrations.RemoveUnusedIndexes do
+  use Ecto.Migration
+
+  def change do
+    drop index(:ideas, [:group_id])
+    drop index(:ideas, [:user_id])
+    drop index(:ideas, [:assignee_id])
+    drop index(:retros, [:facilitator_id])
+  end
+end
