de6dfee7cf6a3507d97a4f35b6eb3eca248ec086
Travis Vander Hoop <vanderhoop@me.com>
2021-11-22 19:58:26 -0700

    Copy: update group-labeling guidance

--- web/static/js/components/stage_change_info_groups_labeling.jsx
+++ web/static/js/components/stage_change_info_groups_labeling.jsx
@@ -8,6 +8,9 @@ export default () => (
         <li>
           Work as a team to arrive at sensible labels for each group of ideas.
         </li>
+        <li>
+          Don't spend too much time labeling any one group. An approximate label is good enough.
+        </li>
       </ul>
     </div>
   </React.Fragment>
