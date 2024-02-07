6486b4fe479b87fec31c555e2d6be7c6ac40d470
Nathan Jones <njones@stride.build>
2024-02-07 12:28:35 -0500

    Update stride urls to stride.build

--- test/services/contact_us_test.js
+++ test/services/contact_us_test.js
@@ -1,49 +1,51 @@
-import ContactUs from "../../web/static/js/services/contact_us"
+import ContactUs from "../../web/static/js/services/contact_us";
 
 describe("ContactUs", () => {
   describe("buildPrepulatedFormForUser", () => {
     const stubUser = {
-      email: "jb.vanderhoop@stridenyc.com",
+      email: "jb.vanderhoop@stride.build",
       family_name: "Vander Hoop",
       given_name: "Joe Bob",
-    }
+    };
 
     it("builds out a query-paramed link to stride's contact us page", () => {
-      const result = ContactUs.buildPrepulatedFormForUser(stubUser)
-      expect(result.startsWith("https://www.stridenyc.com/contact?")).to.eql(true)
-    })
+      const result = ContactUs.buildPrepulatedFormForUser(stubUser);
+      expect(result.startsWith("https://www.stride.build/contact?")).to.eql(
+        true
+      );
+    });
 
     it("identifes the referrer as RemoteRetro", () => {
-      const result = ContactUs.buildPrepulatedFormForUser(stubUser)
-      expect(result).to.contain("referrer=RemoteRetro")
-    })
+      const result = ContactUs.buildPrepulatedFormForUser(stubUser);
+      expect(result).to.contain("referrer=RemoteRetro");
+    });
 
     it("encodes the user's first name as a query parameter", () => {
-      const result = ContactUs.buildPrepulatedFormForUser(stubUser)
-      expect(result).to.contain("firstname=Joe%20Bob")
-    })
+      const result = ContactUs.buildPrepulatedFormForUser(stubUser);
+      expect(result).to.contain("firstname=Joe%20Bob");
+    });
 
     it("encodes the user's last name as a query parameter", () => {
-      const result = ContactUs.buildPrepulatedFormForUser(stubUser)
-      expect(result).to.contain("lastname=Vander%20Hoop")
-    })
+      const result = ContactUs.buildPrepulatedFormForUser(stubUser);
+      expect(result).to.contain("lastname=Vander%20Hoop");
+    });
 
     it("encodes the user's email as a query parameter", () => {
-      const result = ContactUs.buildPrepulatedFormForUser(stubUser)
-      expect(result).to.contain("email=jb.vanderhoop%40stridenyc.com")
-    })
+      const result = ContactUs.buildPrepulatedFormForUser(stubUser);
+      expect(result).to.contain("email=jb.vanderhoop%40stride.build");
+    });
 
     describe("when the user lacks a last name", () => {
       const user = {
         email: "madonna@prince.com",
         given_name: "Madonna",
         family_name: undefined,
-      }
+      };
 
       it("does not include a lastname query param", () => {
-        const result = ContactUs.buildPrepulatedFormForUser(user)
-        expect(result).to.not.contain("lastname")
-      })
-    })
-  })
-})
+        const result = ContactUs.buildPrepulatedFormForUser(user);
+        expect(result).to.not.contain("lastname");
+      });
+    });
+  });
+});
