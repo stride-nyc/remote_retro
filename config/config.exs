5bb1805882634c78132b8370bdacc4cfb4b3945e
dean <deanlloyd64@gmail.com>
2025-02-11 10:44:01 -0500

    update elixir, erlang, node

--- config/config.exs
+++ config/config.exs
@@ -1,9 +1,9 @@
 # This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
+# and its dependencies with the aid of the Config module.
 #
 # This configuration file is loaded before any dependency and
 # is restricted to this project.
-use Mix.Config
+import Config
 
 # General application configuration
 config :remote_retro,
