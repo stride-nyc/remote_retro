34a242484d224db064b4119cb0181ac91a5b6b4e
paytown <paytown@me.com>
2025-03-20 15:42:47 -0400

    simplify ideas_with_ephemeral_grouping_ids

--- web/static/js/services/ideas_with_ephemeral_grouping_ids.js
+++ web/static/js/services/ideas_with_ephemeral_grouping_ids.js
@@ -1,40 +1,10 @@
 export default {
-  // TODO: Figure out if we can skip doing this all together and rely on the temp_group_ids eventually
   buildFrom: ideas => {
-    // Group ideas by temp_group_id
-    const groupedIdeas = {}
-
-    // First pass: create groups based on temp_group_id
-    ideas.forEach(idea => {
-      const tempGroupId = idea.temp_group_id
-
-      // Skip null or undefined temp_group_id
-      if (tempGroupId == null) return
-
-      // Initialize group if it doesn't exist
-      if (!groupedIdeas[tempGroupId]) {
-        groupedIdeas[tempGroupId] = {
-          ephemeralGroupingId: tempGroupId,
-          ideas: [],
-        }
-        console.log(groupedIdeas)
-      }
-
-      // Add idea to its group
-      groupedIdeas[tempGroupId].ideas.push(idea)
-    })
-
-    // Second pass: assign ephemeralGroupingId to each idea
     return ideas.map(idea => {
-      // If idea has a temp_group_id and that group exists
-      if (idea.temp_group_id != null && groupedIdeas[idea.temp_group_id]) {
-        return {
-          ...idea,
-          ephemeralGroupingId: groupedIdeas[idea.temp_group_id].ephemeralGroupingId,
-        }
+      if (idea.temp_group_id != null) {
+        return { ...idea, ephemeralGroupingId: idea.temp_group_id }
       }
 
-      // Ideas without a temp_group_id don't get an ephemeralGroupingId
       return { ...idea }
     })
   },
