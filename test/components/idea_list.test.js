8f6573602a9c688e46d633716d2911d86c5cc0a1
paytown <paytown@me.com>
2025-03-25 15:54:08 -0400

    idea list

--- test/components/idea_list.test.js
+++ test/components/idea_list.test.js
@@ -1,11 +1,20 @@
+/* eslint-disable react/prop-types */
 import React from "react"
-import { shallow } from "enzyme"
+import { render, screen } from "@testing-library/react"
+import "@testing-library/jest-dom"
 
 import IdeaList from "../../web/static/js/components/idea_list"
-import Idea from "../../web/static/js/components/idea"
 import STAGES from "../../web/static/js/configs/stages"
 
 const { IDEA_GENERATION } = STAGES
+
+// Mock the Idea component since we're only testing that IdeaList renders the correct number of
+// Idea components
+jest.mock("../../web/static/js/components/idea", () => {
+  return function MockIdea({ idea }) {
+    return <div data-testid="idea-component" data-idea-id={idea.id}>{idea.body}</div>
+  }
+})
 
 describe("IdeaList", () => {
   const defaultProps = {
@@ -31,14 +40,16 @@ describe("IdeaList", () => {
       vote_count: 1,
     }]
 
-    const wrapper = shallow(
+    render(
       <IdeaList
         {...defaultProps}
         ideasSorted={ideasSorted}
-      />, { disableLifecycleMethods: true }
+      />
     )
 
-    const ideaComponents = wrapper.find(Idea)
-    expect(ideaComponents.length).to.eql(2)
+    const ideaComponents = screen.getAllByTestId("idea-component")
+    expect(ideaComponents).toHaveLength(2)
+    expect(ideaComponents[0]).toHaveTextContent("should be first after stage change alert removed")
+    expect(ideaComponents[1]).toHaveTextContent("should be first at outset")
   })
 })
