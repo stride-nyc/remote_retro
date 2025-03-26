0f4e40dfcf55c67bc83e205c4d9a6fbe56233111
paytown <paytown@me.com>
2025-03-26 09:36:50 -0400

    return retro channel test back to mocha version

--- test/services/retro_channel_test.js
+++ test/services/retro_channel_test.js
@@ -1,10 +1,9 @@
-// TODO: NEED TO FIX THIS TEST
 import { Socket, Channel } from "phoenix"
+import { spy, stub, useFakeTimers } from "sinon"
 import { createStore } from "redux"
 
 import RetroChannel from "../../web/static/js/services/retro_channel"
 import STAGES from "../../web/static/js/configs/stages"
-// TODO: This part not working - come back to this later
 import { setupMockRetroChannel } from "../support/js/test_helper"
 
 const { CLOSED } = STAGES
@@ -12,14 +11,14 @@ const { CLOSED } = STAGES
 describe("RetroChannel", () => {
   let retroChannel
   let initialConnectMethod
-
   // ensure socket#connect is a no-op in tests
-  beforeAll(() => {
+
+  before(() => {
     initialConnectMethod = Socket.prototype.connect
     Socket.prototype.connect = () => {}
   })
 
-  afterAll(() => {
+  after(() => {
     Socket.prototype.connect = initialConnectMethod
   })
 
@@ -31,8 +30,8 @@ describe("RetroChannel", () => {
       })
     })
 
-    test("sets an instance of PhoenixChannel as a client", () => {
-      expect(retroChannel.client.constructor).toEqual(Channel)
+    it("sets an instance of PhoenixChannel as a client", () => {
+      expect(retroChannel.client.constructor).to.equal(Channel)
     })
 
     describe("the returned Phoenix channel channel", () => {
@@ -42,22 +41,22 @@ describe("RetroChannel", () => {
         retroChannelClient = retroChannel.client
       })
 
-      test("is closed", () => {
-        expect(retroChannelClient.state).toEqual(CLOSED)
+      it("is closed", () => {
+        expect(retroChannelClient.state).to.equal(CLOSED)
       })
 
-      test("has a topic attribute identifying the retro with the supplied UUID", () => {
-        expect(retroChannelClient.topic).toEqual("retro:blurg")
+      it("has a topic attribute identifying the retro with the supplied UUID", () => {
+        expect(retroChannelClient.topic).to.equal("retro:blurg")
       })
 
-      test("has a socket attribute referencing a phoenix socket", () => {
-        expect(retroChannelClient.socket.constructor).toEqual(Socket)
+      it("has a socket attribute referencing a phoenix socket", () => {
+        expect(retroChannelClient.socket.constructor).to.equal(Socket)
       })
 
       describe("the socket", () => {
-        test("contains a params object containing the supplied userToken", () => {
+        it("contains a params object containing the supplied userToken", () => {
           const socketParams = retroChannelClient.socket.params()
-          expect(socketParams.userToken).toEqual("38ddm2")
+          expect(socketParams.userToken).to.equal("38ddm2")
         })
       })
     })
@@ -81,17 +80,16 @@ describe("RetroChannel", () => {
       let clock
 
       beforeEach(() => {
-        addIdeaSpy = jest.fn()
-        deleteIdeaSpy = jest.fn()
-        updateIdeaSpy = jest.fn()
-        updatePresenceSpy = jest.fn()
-        updateGroupSpy = jest.fn()
-        updateRetroSpy = jest.fn()
-        voteSubmissionSpy = jest.fn()
-        voteRetractionSpy = jest.fn()
-        setPresencesSpy = jest.fn()
-        jest.useFakeTimers()
-        clock = Date.now()
+        addIdeaSpy = spy()
+        deleteIdeaSpy = spy()
+        updateIdeaSpy = spy()
+        updatePresenceSpy = spy()
+        updateGroupSpy = spy()
+        updateRetroSpy = spy()
+        voteSubmissionSpy = spy()
+        voteRetractionSpy = spy()
+        setPresencesSpy = spy()
+        clock = useFakeTimers(Date.now())
 
         actions = {
           addIdea: addIdeaSpy,
@@ -112,44 +110,41 @@ describe("RetroChannel", () => {
         retroChannelClient = retroChannel.client
       })
 
-      afterEach(() => {
-        jest.useRealTimers()
-      })
-
       describe("on `presence_state`", () => {
-        test("invokes the setPresences action", () => {
+        it("invokes the setPresences action", () => {
           retroChannelClient.trigger("presence_state", {})
-          expect(setPresencesSpy).toHaveBeenCalledTimes(1)
+          expect(setPresencesSpy.calledOnce).to.be.true
         })
       })
 
       describe("on `idea_committed`", () => {
-        test("invokes the addIdea action", () => {
+        it("invokes the addIdea action", () => {
           retroChannelClient.trigger("idea_committed", { body: "zerp" })
-          expect(addIdeaSpy).toHaveBeenCalledWith({ body: "zerp" })
+          expect(addIdeaSpy).calledWith({ body: "zerp" })
         })
       })
 
       describe("on `retro_edited`", () => {
-        test("invokes the retroUpdateCommitted action, passing the payload", () => {
+        it("invokes the retroUpdateCommitted action, passing the payload", () => {
           const payload = { stage: "dummy value" }
           retroChannelClient.trigger("retro_edited", payload)
-          expect(updateRetroSpy).toHaveBeenCalledWith(payload)
+          expect(updateRetroSpy).calledWith(payload)
         })
       })
 
       describe("on `group_edited`", () => {
-        test("invokes the updateGroup action, passing the payload", () => {
+        it("invokes the updateGroup action, passing the payload", () => {
           const payload = { id: 5, label: "Tooling" }
           retroChannelClient.trigger("group_edited", payload)
-          expect(updateGroupSpy).toHaveBeenCalledWith(payload)
+          expect(updateGroupSpy).calledWith(payload)
         })
       })
 
       describe("on `idea_edit_state_enabled`", () => {
-        test("invokes updateIdea with idea id, specifying that the edit is desired by another client", () => {
+        it("invokes updateIdea with idea id, specifying that the edit is desired by another client", () => {
           retroChannelClient.trigger("idea_edit_state_enabled", { id: 2 })
-          expect(updateIdeaSpy).toHaveBeenCalledWith(2, { inEditState: true, isLocalEdit: false })
+
+          expect(updateIdeaSpy).calledWith(2, { inEditState: true, isLocalEdit: false })
         })
       })
 
@@ -158,8 +153,8 @@ describe("RetroChannel", () => {
           retroChannelClient.trigger("idea_edit_state_disabled", { id: 3 })
         })
 
-        test("invokes updateIdea with idea id, passing edit nullification attributes", () => {
-          expect(updateIdeaSpy).toHaveBeenCalledWith(3, { inEditState: false, liveEditText: null })
+        it("invokes updateIdea with idea id, passing edit nullification attributes", () => {
+          expect(updateIdeaSpy).calledWith(3, { inEditState: false, liveEditText: null })
         })
       })
 
@@ -168,8 +163,8 @@ describe("RetroChannel", () => {
           retroChannelClient.trigger("idea_dragged_in_grouping_stage", { id: 3, x: 38.3, y: 91.2 })
         })
 
-        test("invokes updateIdea with drag coordinates and indicator that it's being edited", () => {
-          expect(updateIdeaSpy).toHaveBeenCalledWith(3, { x: 38.3, y: 91.2, inEditState: true })
+        it("invokes updateIdea with drag coordinates and indicator that it's being edited", () => {
+          expect(updateIdeaSpy).calledWith(3, { x: 38.3, y: 91.2, inEditState: true })
         })
       })
 
@@ -177,7 +172,7 @@ describe("RetroChannel", () => {
         beforeEach(() => {
           store = createStore(() => ({
             presences: [
-              { is_typing: true, token: "abc", last_typed: clock },
+              { is_typing: true, token: "abc", last_typed: clock.now },
               { is_typing: false, token: "s0meUserToken" },
             ],
           }))
@@ -187,24 +182,30 @@ describe("RetroChannel", () => {
           retroChannelClient = retroChannel.client
         })
 
-        test("dispatches action for updating the user with matching token to is_typing true with timestamp", () => {
+        afterEach(() => { clock.restore() })
+
+        it("dispatches action for updating the user with matching token to is_typing true with timestamp", () => {
           retroChannelClient.trigger("idea_typing_event", { userToken: "s0meUserToken" })
-          expect(updatePresenceSpy).toHaveBeenCalledWith("s0meUserToken", { is_typing: true, last_typed: clock })
+
+          expect(updatePresenceSpy).calledWith("s0meUserToken", { is_typing: true, last_typed: clock.now })
         })
 
         describe("when the user with matching token has already typed", () => {
-          test("dispatches action for updating the user with matching token to is_typing false after a delay", () => {
+          it("dispatches action for updating the user with matching token to is_typing false after a delay", () => {
             retroChannelClient.trigger("idea_typing_event", { userToken: "abc" })
-            jest.advanceTimersByTime(900)
-            expect(updatePresenceSpy).toHaveBeenCalledWith("abc", { is_typing: false })
+            clock.tick(900)
+
+            expect(updatePresenceSpy).calledWith("abc", { is_typing: false })
           })
 
-          test("delays setting `is_typing` back to false if the event is received again", () => {
+          it("delays setting `is_typing` back to false if the event is received again", () => {
             retroChannelClient.trigger("idea_typing_event", { userToken: "abc" })
-            jest.advanceTimersByTime(400)
+            clock.tick(400)
+            clock.restore() // necessary, as Date.now is used at 10ms interval in implementation
+            clock = useFakeTimers(Date.now())
             retroChannelClient.trigger("idea_typing_event", { userToken: "abc" })
-            jest.advanceTimersByTime(500)
-            expect(updatePresenceSpy).not.toHaveBeenCalledWith("abc", { is_typing: false })
+            clock.tick(500)
+            expect(updatePresenceSpy).not.calledWith("abc", { is_typing: false })
           })
         })
 
@@ -221,11 +222,11 @@ describe("RetroChannel", () => {
             retroChannelClient = retroChannel.client
           })
 
-          test("does not throw an error", () => {
+          it("does not throw an error", () => {
             expect(() => {
               retroChannelClient.trigger("idea_typing_event", { userToken: "tokenRepresentingUserNotCurrentlyPresent" })
-              jest.advanceTimersByTime(900)
-            }).not.toThrow()
+              clock.tick(900)
+            }).to.not.throw()
           })
         })
       })
@@ -235,15 +236,15 @@ describe("RetroChannel", () => {
           retroChannelClient.trigger("idea_live_edit", { id: 2, liveEditText: "lalala" })
         })
 
-        test("invokes the updateIdea action with idea id, passing the `liveEditText` value along", () => {
-          expect(updateIdeaSpy).toHaveBeenCalledWith(2, { id: 2, liveEditText: "lalala" })
+        it("invokes the updateIdea action with idea id, passing the `liveEditText` value along", () => {
+          expect(updateIdeaSpy).calledWith(2, { id: 2, liveEditText: "lalala" })
         })
       })
 
       describe("on `idea_deleted`", () => {
-        test("invokes deleteIdea action, passing in the idea's id", () => {
+        it("invokes deleteIdea action, passing in the idea's id", () => {
           retroChannelClient.trigger("idea_deleted", { id: 6 })
-          expect(deleteIdeaSpy).toHaveBeenCalledWith(6)
+          expect(deleteIdeaSpy).calledWith(6)
         })
       })
 
@@ -252,8 +253,8 @@ describe("RetroChannel", () => {
           retroChannelClient.trigger("idea_edited", { id: 2, body: "i like TEENAGE MUTANT NINJA TURTLES" })
         })
 
-        test("invokes updateIdea action, passing idea id & nulling the editing attributes", () => {
-          expect(updateIdeaSpy).toHaveBeenCalledWith(2, {
+        it("invokes updateIdea action, passing idea id & nulling the editing attributes", () => {
+          expect(updateIdeaSpy).calledWith(2, {
             id: 2,
             body: "i like TEENAGE MUTANT NINJA TURTLES",
             inEditState: false,
@@ -272,8 +273,8 @@ describe("RetroChannel", () => {
           })
         })
 
-        test("invokes the voteSubmission action, passing the vote", () => {
-          expect(voteSubmissionSpy).toHaveBeenCalledWith({
+        it("invokes the voteSubmission action, passing the vote", () => {
+          expect(voteSubmissionSpy).calledWith({
             idea_id: 50,
             user_id: 99,
           })
@@ -285,8 +286,8 @@ describe("RetroChannel", () => {
           retroChannelClient.trigger("vote_retracted", { id: 21 })
         })
 
-        test("invokes the voteRetraction action, passing the vote", () => {
-          expect(voteRetractionSpy).toHaveBeenCalledWith({ id: 21 })
+        it("invokes the voteRetraction action, passing the vote", () => {
+          expect(voteRetractionSpy).calledWith({ id: 21 })
         })
       })
     })
@@ -295,24 +296,26 @@ describe("RetroChannel", () => {
   describe("#join", () => {
     beforeEach(() => {
       retroChannel = new RetroChannel({})
-      retroChannel.client = { join: jest.fn() }
+      retroChannel.client = { join: spy() }
+
       retroChannel.join()
     })
 
-    test("delegates to the client's join method", () => {
-      expect(retroChannel.client.join).toHaveBeenCalled()
+    it("delegates to the client's join method", () => {
+      expect(retroChannel.client.join).to.have.been.called
     })
   })
 
   describe("#push", () => {
     beforeEach(() => {
       retroChannel = new RetroChannel({})
-      retroChannel.client = { push: jest.fn() }
+      retroChannel.client = { push: spy() }
+
       retroChannel.push("bizarreStringToPush")
     })
 
-    test("delegates to the client's push method, forwarding the arguments", () => {
-      expect(retroChannel.client.push).toHaveBeenCalledWith("bizarreStringToPush")
+    it("delegates to the client's push method, forwarding the arguments", () => {
+      expect(retroChannel.client.push).to.have.been.calledWith("bizarreStringToPush")
     })
   })
 
@@ -323,85 +326,125 @@ describe("RetroChannel", () => {
     beforeEach(() => {
       // use mock channel, as we need to trigger receives on the channel to verify callbacks
       retroChannel = setupMockRetroChannel()
-      mockCallbacks = { onOk: jest.fn(), onErr: jest.fn() }
-      pushSpy = jest.spyOn(retroChannel, "push")
+      mockCallbacks = { onOk: spy(), onErr: spy() }
+      pushSpy = spy(retroChannel, "push")
       retroChannel.pushWithRetries("derp", { some: "values" }, mockCallbacks)
     })
 
     afterEach(() => {
-      pushSpy.mockRestore()
+      pushSpy.restore()
     })
 
-    test("calls push with the given message and payload", () => {
-      expect(pushSpy).toHaveBeenCalledWith("derp", { some: "values" })
+    it("calls push with the given message and payload", () => {
+      expect(pushSpy).to.have.been.calledWithMatch("derp", { some: "values" })
     })
 
     describe("when the push results in a success response", () => {
       let okSpy
 
       beforeEach(() => {
-        okSpy = jest.fn()
+        okSpy = spy()
         retroChannel = setupMockRetroChannel()
         retroChannel.pushWithRetries("derp", { some: "values" }, { onOk: okSpy, onErr: () => {} })
+
         retroChannel.__triggerReply("ok", { id: 101 })
       })
 
-      test("invokes the onOk callback with the given payload", () => {
-        expect(okSpy).toHaveBeenCalledWith({ id: 101 })
+      it("invokes the onOk callback with the given payload", () => {
+        expect(okSpy).to.have.been.calledWithMatch({ id: 101 })
       })
     })
 
     describe("when the push results in an error response", () => {
+      let clock
+      let spyHook
+
       beforeEach(() => {
-        jest.useFakeTimers()
+        clock = useFakeTimers()
         retroChannel = setupMockRetroChannel()
-        pushSpy = jest.spyOn(retroChannel, "push")
+
+        spyHook = {}
+        addRetrySpyToPushResult(retroChannel, spyHook)
+
         retroChannel.pushWithRetries("what", { some: "gives" }, mockCallbacks)
+
         retroChannel.__triggerReply("error", { one: "five" })
       })
 
       afterEach(() => {
-        pushSpy.mockRestore()
-        jest.useRealTimers()
+        retroChannel.push.restore()
+        clock.restore()
       })
 
-      test("retries the push only after a brief timeout", () => {
-        expect(pushSpy).toHaveBeenCalledTimes(1)
-        jest.advanceTimersByTime(1000)
-        expect(pushSpy).toHaveBeenCalledTimes(2)
+      it("retries the push only after a brief timeout", () => {
+        expect(() => {
+          clock.tick(1000)
+        }).to.alter(() => (spyHook.retrySpy.called), {
+          from: false,
+          to: true,
+        })
       })
 
       describe("when the retry results in an error response", () => {
-        test("triggers a *second* retry after a longer timeout", () => {
-          jest.advanceTimersByTime(1000)
-          retroChannel.__triggerReply("error", { one: "two" })
-          jest.advanceTimersByTime(3000)
-          expect(pushSpy).toHaveBeenCalledTimes(3)
+        it("triggers a *second* retry after a longer timeout", () => {
+          clock.tick(1000)
+
+          expect(() => {
+            retroChannel.__triggerReply("error", { one: "two" })
+            clock.tick(3000)
+          }).to.alter(() => (spyHook.retrySpy.callCount), {
+            from: 1,
+            to: 2,
+          })
         })
       })
 
       describe("when the second retry results in an error response", () => {
-        test("triggers a *third* retry after a longer timeout", () => {
-          jest.advanceTimersByTime(1000)
+        it("triggers a *third* retry after a longer timeout", () => {
+          clock.tick(1000)
           retroChannel.__triggerReply("error", {})
-          jest.advanceTimersByTime(3000)
-          retroChannel.__triggerReply("error", {})
-          jest.advanceTimersByTime(5000)
-          expect(pushSpy).toHaveBeenCalledTimes(4)
+          clock.tick(3000)
+
+          expect(() => {
+            retroChannel.__triggerReply("error", {})
+            clock.tick(5000)
+          }).to.alter(() => (spyHook.retrySpy.callCount), {
+            from: 2,
+            to: 3,
+          })
         })
       })
 
       describe("when the third retry results in an error response", () => {
-        test("invokes the onErr callback with error payload", () => {
-          jest.advanceTimersByTime(1000)
+        it("invokes the onErr callback with error payload", () => {
+          clock.tick(1000)
           retroChannel.__triggerReply("error", {})
-          jest.advanceTimersByTime(3000)
+          clock.tick(3000)
           retroChannel.__triggerReply("error", {})
-          jest.advanceTimersByTime(5000)
-          retroChannel.__triggerReply("error", { fart: "store" })
-          expect(mockCallbacks.onErr).toHaveBeenCalledWith({ fart: "store" })
+          clock.tick(5000)
+
+          expect(() => {
+            retroChannel.__triggerReply("error", { fart: "store" })
+          }).to.alter(() => (
+            mockCallbacks.onErr.calledWithMatch({ fart: "store" })
+          ), {
+            from: false,
+            to: true,
+          })
         })
       })
     })
   })
 })
+
+const addRetrySpyToPushResult = (retroChannel, hook) => {
+  const originalPushImplementation = retroChannel.push.bind(retroChannel)
+
+  stub(retroChannel, "push", (...args) => {
+    const push = originalPushImplementation(...args)
+
+    hook.retrySpy = spy(push, "send")
+
+    return push
+  })
+}
