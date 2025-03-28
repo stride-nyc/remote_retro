a3ac6f6e1b15176cf9d36348068e89b70dfffd3a
paytown <paytown@me.com>
2025-03-28 11:43:54 -0400

    notes

--- web/static/js/components/grouping_stage.jsx
+++ web/static/js/components/grouping_stage.jsx
@@ -65,14 +65,14 @@ GroupingStage.propTypes = {
 
 export default GroupingStage
 
-// Setup a11y
+// x - Setup a11y
 // x - Figure out collsion detection
 // x - Groupings based off collision detection
 // Remove old stuff, including tests
 // Tests
 // x - Location of cards persists when nav off page
 // x - Styles
-// NICE TO HAVE: All users see the dragging cards and groupings live - not local state
-// need to test - disable card when dragged by another user until drag stops
+// NICE TO HAVE (New ticket?): All users see the dragging cards and groupings live - not local state
+// x - disable card when dragged by another user until drag stops
 // x- Groupings not working on on non dragging screen.
 // Consider pulling out any references to ephemeralIds - wip branch: pl/dnd-kit-no-ephemeral-ids
