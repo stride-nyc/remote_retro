185c6f760a1988a5e5668b149e409dd974109748
paytown <paytown@me.com>
2025-04-03 15:27:54 -0400

    add dnd-kit dep

--- web/static/js/components/columnar_board_layout.jsx
+++ web/static/js/components/columnar_board_layout.jsx
@@ -1,18 +1,22 @@
 import React from "react"
+import { DndContext } from "@dnd-kit/core"
 
 import CategoryColumn from "./category_column"
 
 import * as AppPropTypes from "../prop_types"
 import styles from "./css_modules/columnar_board_layout.css"
 
+// Look here
 const ColumnarBoardLayout = props => {
   const { categories } = props
 
   return (
     <div className={styles.categoryColumnsWrapper}>
-      {categories.map(category => (
-        <CategoryColumn {...props} category={category} key={category} />
-      ))}
+      <DndContext>
+        {categories.map(category => (
+          <CategoryColumn {...props} category={category} key={category} />
+        ))}
+      </DndContext>
     </div>
   )
 }
