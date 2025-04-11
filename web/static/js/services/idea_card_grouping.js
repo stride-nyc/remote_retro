fd452cb445d38c3260cffc150706df0915e2b9ae
paytown <paytown@me.com>
2025-04-11 15:10:06 -0400

    clarity

--- web/static/js/services/idea_card_grouping.js
+++ web/static/js/services/idea_card_grouping.js
@@ -1,15 +1,3 @@
-const batchGetBoundingClientRects = cardRefs => {
-  const rects = {}
-
-  Object.entries(cardRefs).forEach(([id, ref]) => {
-    if (ref) {
-      rects[Number(id)] = ref.getBoundingClientRect()
-    }
-  })
-
-  return rects
-}
-
 export default {
   findConnectedGroups: cardRefs => {
     const cardIds = Object.keys(cardRefs).map(Number)
@@ -53,6 +41,18 @@ const areElementsOverlapping = (rect1, rect2) => {
     && rect1.bottom > rect2.top
     && rect1.top < rect2.bottom
   )
+}
+
+const batchGetBoundingClientRects = cardRefs => {
+  const rects = {}
+
+  Object.entries(cardRefs).forEach(([id, ref]) => {
+    if (ref) {
+      rects[Number(id)] = ref.getBoundingClientRect()
+    }
+  })
+
+  return rects
 }
 
 const findOverlappingElements = (activeId, rects) => {
