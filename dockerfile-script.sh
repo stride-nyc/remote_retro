5eb8363d0562d003274ecdd31fbbdc9ddf90430b
JohnJasonFisher <jjfisher91@gmail.com>
2026-03-19 15:33:41 -0500

    Add yarn to Dockerfile

--- dockerfile-script.sh
+++ dockerfile-script.sh
@@ -5,7 +5,6 @@ set -e  # Exit on error
 apt-get update && apt-get install -y netcat inotify-tools
 
 # install openssl 1.1
-
 cd /usr/local/bin
 wget https://www.openssl.org/source/openssl-1.1.1n.tar.gz
 tar -xvzf openssl-1.1.1n.tar.gz
@@ -15,5 +14,13 @@ make
 make install
 
 # install nvm and npm (and make available)
-
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+# install project version of node (24.14.0) and use it
+nvm install 24.14.0 && nvm use 24.14.0
+
+# install yarn
+npm install -g yarn
+
+# install dependencies
+yarn install
